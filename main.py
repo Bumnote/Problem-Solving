6e1a6d6eadf5099f0cc0ae76c9f45d0b55608d56
YongBum <dt10002@naver.com>
2023-06-06 00:00:58 +0900

    23.06.06

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-def solution(s):
-    zero_cnt = 0  # 없앤 0의 개수
-    cnt = 0  # 변환 횟수
+tc = int(input())  # 테스트 케이스
 
-    # "1"이 될 때까지 반복
-    while s != "1":
-        cnt += 1  # 변환 횟수 증가
-        zero_cnt += s.count("0")  # 0의 개수 더하기
-        temp = "".join([elem for elem in s if elem == "1"])  # 문자열 s에서 0을 모두 빼낸 결과
-        s = bin(len(temp))[2:]  # 2진수로 변환
+for t in range(1, tc + 1):
+    ## 변수 입력 부분 ##
+    n, m = map(int, input().split())  # n x n 배열, m x m 파리채
+    grid = [list(map(int, input().split())) for _ in range(n)]
+    answer = 0
+    Sum = 0  # 파리채 영역마다 죽은 파리 수
 
-    return [cnt, zero_cnt]
+    ## 문제 해결 부분 ##
+    # n x n 배열에서 m x m 배열을 탐색
+    for y in range(n - m + 1):
+        for x in range(n - m + 1):
+            # 해당 위치에서 m x m 배열을 탐색
+            for z in range(m):
+                for w in range(m):
+                    Sum += grid[y + z][x + w]
+            if answer < Sum:
+                answer = Sum
+            Sum = 0  # 초기화
+
+    print(f"#{t} {answer}")
