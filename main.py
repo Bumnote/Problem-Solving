a6d333e42794db71e62e2663193a0efabd6b1177
YongBum <dt10002@naver.com>
2023-06-07 22:21:26 +0900

    23.06.07

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from sys import stdin
+tc = int(input().strip())  # tc: 테스트 케이스
+temp = "abcdefghijklmnopqrstuvwxyz"  # 비교 문자열
 
-input = stdin.readline
+for t in range(1, tc + 1):
+    ## 변수 입력 부분 ##
+    s = input()
 
-## 변수 입력 부분 ##
-tc = int(input().strip())  # 테스트 케이스
+    ## 문제 풀이 부분 ##
+    cnt = 0
+    for i in range(len(s)):
+        if temp[i] == s[i]:
+            cnt += 1
+        else:
+            break
 
-## 문제 해결 부분 ##
-for _ in range(tc):
-    dp = [1, 2, 4]  # dp 초기화
-    n = int(input().strip())  # 1 <= n < 11
-    for i in range(n - 3):
-        next = dp[i] + dp[i + 1] + dp[i + 2]  # dp 점화식
-        dp.append(next)  # dp 리스트에 append
-
-    print(dp[n - 1])
+    print(f"#{t} {cnt}")
