c81b252cd4fbec2bd7ea4e3b411fde042fe67157
DT <88239689+Bumnote@users.noreply.github.com>
2023-06-10 13:49:26 +0900

    [Silver I] Title: 효율적인 해킹, Time: 11768 ms, Memory: 215140 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
@@ -1,49 +1,37 @@
-from sys import stdin
+import sys
 from collections import deque
-
-input = stdin.readline
-
-## 변수 입력 부분 ##
-n, m = map(int, input().split())  # n: 정점, m: 간선 (n <= 10,000 / m <= 10,000)
-vertex = [[] for _ in range(n + 1)]
-for _ in range(m):
-    A, B = map(int, input().split())  # A -> B 신뢰: B 해킹 -> A 해킹 (단방향)
-    vertex[B].append(A)
+n, m = map(int, sys.stdin.readline().split())
+graph = [[] for _ in range(n+1)]
+ans = 0
 
 
-## 문제 해결 부분 ##
-def bfs(x):
-    global cnt
-    deq = deque()
-    deq.append(x)
-    visited[x] = False  # 방문 처리
+def BFS(start):
+    visited = [0 for _ in range(n+1)]
+    visited[start] = 1
+    cnt = 1
+    queue = deque()
+    queue.append(start)
 
-    while deq:
-        cur_x = deq.popleft()
-
-        for elem in vertex[cur_x]:
-            # 방문이 가능하다면 -> deq에 추가 & 해킹 가능 횟수 증가
-            if visited[elem]:
-                visited[elem] = False  # 방문 처리
-                deq.append(elem)
+    while queue:
+        v = queue.popleft()
+        for j in graph[v]:
+            if visited[j] == 0:
+                visited[j] = 1
+                queue.append(j)
                 cnt += 1
+    return cnt
 
 
-max_hack = 0
-hack_lst = []
-for i in range(1, n + 1):
-    visited = [True] * (n + 1)
-    cnt = 0
+for _ in range(m):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[b].append(a)
 
-    bfs(i)  # 너비 우선 탐색 실행
+for i in range(1, n+1):
+    x = BFS(i)
+    if x > ans:
+        ans = x
+        lst = [i]
+    elif x == ans:
+        lst.append(i)
 
-    if max_hack < cnt:
-        hack_lst = [i]
-        max_hack = cnt  # 최대 해킹 가능 횟수 갱신
-
-    # 최대 해킹 가능 횟수가 같으면 hack_lst에 계속 추가 
-    elif max_hack == cnt:
-        hack_lst.append(i)
-
-## 정답 출력 부분 ##
-print(*hack_lst)
+print(*lst)
