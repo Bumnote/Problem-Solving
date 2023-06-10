96bd069dffabf68680ab45308df024b405f0f2ad
DT <88239689+Bumnote@users.noreply.github.com>
2023-06-10 13:26:46 +0900

    [Silver I] Title: 효율적인 해킹, Time: 14500 ms, Memory: 215416 KB -BaekjoonHub

---
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
@@ -0,0 +1,47 @@
+from sys import stdin
+from collections import deque
+
+input = stdin.readline
+
+## 변수 입력 부분 ##
+n, m = map(int, input().split())  # n: 정점, m: 간선 (n <= 10,000 / m <= 10,000)
+vertex = [[] for _ in range(n + 1)]
+for _ in range(m):
+    A, B = map(int, input().split())  # A -> B 신뢰: B 해킹 -> A 해킹 (단방향)
+    vertex[B].append(A)
+
+
+## 문제 해결 부분 ##
+def bfs(x):
+    global cnt
+    deq = deque()
+    deq.append(x)
+    visited[x] = False  # 방문 처리
+
+    while deq:
+        cur_x = deq.popleft()
+
+        for elem in vertex[cur_x]:
+            # 방문이 가능하다면 -> deq에 추가 & 해킹 가능 횟수 증가 
+            if visited[elem]:
+                visited[elem] = False  # 방문 처리
+                deq.append(elem)
+                cnt += 1
+
+
+max_hack = 0
+hack_cnt = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    visited = [True] * (n + 1)
+    cnt = 0
+
+    bfs(i)  # 너비 우선 탐색 실행 
+
+    hack_cnt[i] = cnt
+    max_hack = max(max_hack, cnt)  # 최대 해킹 횟수 갱신 
+
+## 정답 출력 부분 ##
+for j in range(1, n + 1):
+    if hack_cnt[j] == max_hack:
+        print(j, end=" ")
