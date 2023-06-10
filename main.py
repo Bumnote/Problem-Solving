aff87c0806d46f5bf93c88bb8ef70bc681048d91
YongBum <dt10002@naver.com>
2023-06-11 08:23:36 +0900

    23.06.11

--- main.py
+++ main.py
@@ -3,48 +3,31 @@ from collections import deque
 
 input = stdin.readline
 
-## 변수 입력 부분 ##
-n, m = map(int, input().split())  # n: 정점, m: 간선 (n <= 10,000 / m <= 10,000)
-vertex = [[] for _ in range(n + 1)]
-for _ in range(m):
-    A, B = map(int, input().split())  # A -> B 신뢰: B 해킹 -> A 해킹 (단방향)
-    vertex[B].append(A)
 
-
-## 문제 해결 부분 ##
 def bfs(x):
-    global cnt
     deq = deque()
-    deq.append(x)
+    deq.append((0, 0))
     visited[x] = False  # 방문 처리
 
     while deq:
-        cur_x = deq.popleft()
+        cur_x, cur_cnt = deq.popleft()
+        if cur_x == n - 1:
+            return cur_cnt
 
-        for elem in vertex[cur_x]:
-            # 방문이 가능하다면 -> deq에 추가 & 해킹 가능 횟수 증가
-            if visited[elem]:
-                visited[elem] = False  # 방문 처리
-                deq.append(elem)
-                cnt += 1
+        for i in range(1, n_list[cur_x] + 1):
+            new_x = cur_x + i
+            # 방문이 가능하면 -> deq에 추가
+            if 0 <= new_x < n and visited[new_x]:
+                deq.append((new_x, cur_cnt + 1))
+                visited[new_x] = False  # 방문 처리
+
+    return -1
 
 
-max_hack = 0
-# hack_lst = []
-for i in range(1, n + 1):
-    visited = [True] * (n + 1)
-    cnt = 0
+## 변수 입력 부분 ##
+n = int(input().strip())  # 1 x n 미로 (1 <= n <= 1,000)
+n_list = list(map(int, input().split()))  # 미로
+visited = [True] * n  # 방문 확인용
 
-    bfs(i)  # 너비 우선 탐색 실행
-
-    if max_hack < cnt:
-        hack_lst = [i]
-        max_hack = cnt  # 최대 해킹 가능 횟수 갱신
-
-    # 최대 해킹 가능 횟수가 같으면 hack_lst에 계속 추가
-    elif max_hack == cnt:
-        hack_lst.append(i)
-
-## 정답 출력 부분 ##
-print(*hack_lst)
-
+## 문제 해결 부분 ##
+print(bfs(0))
