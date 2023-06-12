f8fee841f1cc0393589a826f5c78d4b9117616a4
YongBum <dt10002@naver.com>
2023-06-12 23:41:18 +0900

    23.06.12

--- main.py
+++ main.py
@@ -1,33 +1,46 @@
-from sys import stdin
-from collections import deque
+from sys import stdin, setrecursionlimit
 
+setrecursionlimit(10 ** 6)
 input = stdin.readline
 
-
-def bfs(x):
-    deq = deque()
-    deq.append((0, 0))
-    visited[x] = False  # 방문 처리
-
-    while deq:
-        cur_x, cur_cnt = deq.popleft()
-        if cur_x == n - 1:
-            return cur_cnt
-
-        for i in range(1, n_list[cur_x] + 1):
-            new_x = cur_x + i
-            # 방문이 가능하면 -> deq에 추가
-            if 0 <= new_x < n and visited[new_x]:
-                deq.append((new_x, cur_cnt + 1))
-                visited[new_x] = False  # 방문 처리
-
-    return -1
-
-
 ## 변수 입력 부분 ##
-n = int(input().strip())  # 1 x n 미로 (1 <= n <= 1,000)
-n_list = list(map(int, input().split()))  # 미로
-visited = [True] * n  # 방문 확인용
+n, m = map(int, input().split())  # n x m 행렬
+c_map = [list(map(int, input().split())) for _ in range(n)]
+t = int(input().strip())
 
 ## 문제 해결 부분 ##
-print(bfs(0))
+color = [[] for _ in range(n)]
+visited = [[True] * m for _ in range(n)]
+
+for y in range(n):
+    for x in range(0, m * 3 - 2, 3):
+        total = 0
+        for i in range(3):
+            total += c_map[y][x + i]
+
+        if total >= t * 3:
+            color[y].append(255)
+        else:
+            color[y].append(0)
+
+
+def dfs(y, x):
+    for dy, dx in ((-1, 0), (1, 0), (0, 1), (0, -1)):
+        new_y, new_x = y + dy, x + dx
+        # 범위를 넘거나, 이미 방문했거나, 픽셀값이 255가 아니면 -> continue
+        if new_y < 0 or new_y >= n or new_x < 0 or new_x >= m or not visited[new_y][new_x] or color[new_y][new_x] != 255:
+            continue
+        visited[new_y][new_x] = False  # 방문 처리
+        dfs(new_y, new_x)
+
+
+answer = 0
+for y in range(n):
+    for x in range(m):
+        # 방문 가능하고, 픽셀값이 255이면 -> dfs 탐색
+        if visited[y][x] and color[y][x] == 255:
+            visited[y][x] = False  # 방문 처리
+            dfs(y, x)
+            answer += 1
+
+print(answer)
