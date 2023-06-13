dd27e5c39f2867771202890ad42b7dd3410ab588
YongBum <dt10002@naver.com>
2023-06-13 15:20:11 +0900

    23.06.13

--- main.py
+++ main.py
@@ -1,46 +1,38 @@
-from sys import stdin, setrecursionlimit
+from sys import stdin
 
-setrecursionlimit(10 ** 6)
 input = stdin.readline
 
 ## 변수 입력 부분 ##
-n, m = map(int, input().split())  # n x m 행렬
-c_map = [list(map(int, input().split())) for _ in range(n)]
-t = int(input().strip())
+r, c, k = map(int, input().split())  # r: 행, c: 열, k: 거리
+h_map = [list(input().strip()) for _ in range(r)]
+
 
 ## 문제 해결 부분 ##
-color = [[] for _ in range(n)]
-visited = [[True] * m for _ in range(n)]
+## 왼쪽 아래(현수) ----> 오른쪽 위(집)
+def dfs(y, x, k):
+    global cnt, answer
 
-for y in range(n):
-    for x in range(0, m * 3 - 2, 3):
-        total = 0
-        for i in range(3):
-            total += c_map[y][x + i]
+    # 현수가 집에 도착한 경우 -> answer++
+    if y == 0 and x == c - 1 and cnt == k - 1:
+        answer += 1
+        return
 
-        if total >= t * 3:
-            color[y].append(255)
-        else:
-            color[y].append(0)
-
-
-def dfs(y, x):
-    for dy, dx in ((-1, 0), (1, 0), (0, 1), (0, -1)):
+    for dy, dx in ((-1, 0), (0, 1), (1, 0), (0, -1)):
         new_y, new_x = y + dy, x + dx
-        # 범위를 넘거나, 이미 방문했거나, 픽셀값이 255가 아니면 -> continue
-        if new_y < 0 or new_y >= n or new_x < 0 or new_x >= m or not visited[new_y][new_x] or color[new_y][new_x] != 255:
+
+        if new_y < 0 or new_y >= r or new_x < 0 or new_x >= c or not visited[new_y][new_x] or h_map[new_y][
+            new_x] == "T":
             continue
         visited[new_y][new_x] = False  # 방문 처리
-        dfs(new_y, new_x)
+        cnt += 1
+        dfs(new_y, new_x, k)
+        cnt -= 1
+        visited[new_y][new_x] = True  # 복원 처리
 
 
-answer = 0
-for y in range(n):
-    for x in range(m):
-        # 방문 가능하고, 픽셀값이 255이면 -> dfs 탐색
-        if visited[y][x] and color[y][x] == 255:
-            visited[y][x] = False  # 방문 처리
-            dfs(y, x)
-            answer += 1
+visited = [[True] * c for _ in range(r)]  # 방문 확인용
+cnt, answer = 0, 0
+visited[r - 1][0] = False  # 현수의 위치 -> 방문처리
+dfs(r - 1, 0, k)
 
 print(answer)
