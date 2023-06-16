33ab1009e80668b914c57a0638028b98b35381c8
YongBum <dt10002@naver.com>
2023-06-16 13:36:34 +0900

    23.06.16

--- main.py
+++ main.py
@@ -1,34 +1,28 @@
-from sys import stdin, setrecursionlimit
+from sys import stdin
 
 input = stdin.readline
-setrecursionlimit(10 ** 5)
 
 ## 변수 입력 부분 ##
-n, m = map(int, input().split())  # n: 작업 개수, m: 순서 정보
-job = [[] for _ in range(n + 1)]
-
-for _ in range(m):
-    a, b = map(int, input().split())  # a -> b 단방향 그래프
-    job[b].append(a)  # b -> a 로 저장하여 b에서부터 dfs 실행
-
-x = int(input().strip())  # x: 오늘 끝내야 할 작업 번호
+n_list = [list(map(str, input().split())) for _ in range(5)]
+n_set = set() # 중복 제거를 위해서 set 자료형 선언
 
 
 ## 문제 해결 부분 ##
-def dfs(x):
-    global cnt
+def dfs(y, x, cnt, s):
+    # 6 자리의 문자열을 만들었다면 -> set에 추가 후 return
+    if cnt == 6:
+        n_set.add(s)
+        return
 
-    for elem in job[x]:
-        # 방문이 가능하다면 -> 깊이 탐색
-        if visited[elem]:
-            visited[elem] = False  # 방문 처리
-            cnt += 1
-            dfs(elem)
+    for dy, dx in ((-1, 0), (1, 0), (0, 1), (0, -1)):
+        new_y, new_x = y + dy, x + dx,
+        if new_y < 0 or new_y >= 5 or new_x < 0 or new_x >= 5:
+            continue
+        dfs(new_y, new_x, cnt + 1, s + n_list[new_y][new_x])
 
 
-visited = [True] * (n + 1)  # 방문 확인용
-visited[x] = False  # 방문 처리
-cnt = 0
-dfs(x)
+for y in range(5):
+    for x in range(5):
+        dfs(y, x, 0, "")  # 깊이 우선 탐색
 
-print(cnt)
+print(len(n_set))
