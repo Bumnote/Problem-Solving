3e9bba34c275ae12fb40a173d00bdc841f9d831a
YongBum <dt10002@naver.com>
2023-06-17 14:51:08 +0900

    23.06.17

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
-from sys import stdin
+from sys import stdin, setrecursionlimit
 
+setrecursionlimit(10 ** 4)
 input = stdin.readline
 
-## 변수 입력 부분 ##
-n_list = [list(map(str, input().split())) for _ in range(5)]
-n_set = set() # 중복 제거를 위해서 set 자료형 선언
+tc = int(input().strip())  # 테스트 케이스 입력
+for _ in range(tc):
+    ## 변수 입력 부분 ##
+    n, m = map(int, input().split())  # n: 행, m: 열
+    field = [list(input().strip()) for _ in range(n)]
+    visited = [[True] * m for _ in range(n)]
 
 
-## 문제 해결 부분 ##
-def dfs(y, x, cnt, s):
-    # 6 자리의 문자열을 만들었다면 -> set에 추가 후 return
-    if cnt == 6:
-        n_set.add(s)
-        return
+    ## 문제 해결 부분 ##
+    def dfs(y, x):
 
-    for dy, dx in ((-1, 0), (1, 0), (0, 1), (0, -1)):
-        new_y, new_x = y + dy, x + dx,
-        if new_y < 0 or new_y >= 5 or new_x < 0 or new_x >= 5:
-            continue
-        dfs(new_y, new_x, cnt + 1, s + n_list[new_y][new_x])
+        for dy, dx in ((-1, 0), (1, 0), (0, 1), (0, -1)):
+            new_y, new_x = y + dy, x + dx
+
+            if new_y < 0 or new_y >= n or new_x < 0 or new_x >= m or not visited[new_y][new_x] or field[new_y][
+                new_x] == ".":
+                continue
+
+            visited[new_y][new_x] = False  # 방문 처리
+            dfs(new_y, new_x)
 
 
-for y in range(5):
-    for x in range(5):
-        dfs(y, x, 0, "")  # 깊이 우선 탐색
+    cnt = 0
+    for y in range(n):
+        for x in range(m):
+            # 방문 가능하고, 양(#)이라면 -> dfs 탐색
+            if visited[y][x] and field[y][x] == "#":
+                visited[y][x] = False  # 방문 처리
+                dfs(y, x)
+                cnt += 1
 
-print(len(n_set))
+    print(cnt)
