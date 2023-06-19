2f6704feb8c9a832d3ed040403bba779f2754632
YongBum <dt10002@naver.com>
2023-06-19 18:09:34 +0900

    23.06.19

--- main.py
+++ main.py
@@ -1,32 +1,29 @@
 from sys import stdin
-from math import isqrt
 
 input = stdin.readline
 
 ## 변수 입력 부분 ##
-n = int(input().strip())  # n자리 수, (1 <= n <= 8)
-odds = ["1", "3", "5", "7", "9"]
+n, k = map(int, input().split())  # n까지, k번째 지우는 수
+primes = [False, False] + [True] * (n - 1)
+
 
 ## 문제 해결 부분 ##
-def isPrime(num):
-    for i in range(2, isqrt(num) + 1):
-        if num % i == 0:
-            return False
-    # for 반복문에 걸리지 않으면 소수
-    return True
+def solve():
+    global cnt
+    for i in range(2, len(primes)):
+        if primes[i]:
+            cnt += 1
+            if cnt == k:
+                return i
+            for j in range(2 * i, len(primes), i):
+                # 이미 처리했으면 횟수에 포함하지 않는다.
+                if not primes[j]:
+                    continue
+                primes[j] = False  # 배수는 소수가 아님
+                cnt += 1
+                if cnt == k:
+                    return j
 
-def dfs(x):
-    # 길이가 n이 된 경우 -> print
-    if len(x) == n:
-        print(x)
-        return
 
-    for odd in odds:
-        if isPrime(int(x + odd)):
-            dfs(x + odd)
-
-
-dfs("2")
-dfs("3")
-dfs("5")
-dfs("7")
+cnt = 0
+print(solve())
