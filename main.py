f7e46373c52ea2101794ea8620fc99f057ecc3a1
YongBum <dt10002@naver.com>
2023-06-26 17:02:24 +0900

    23.06.26

--- main.py
+++ main.py
@@ -1,30 +1,44 @@
 from sys import stdin
+from collections import deque
 
 input = stdin.readline
 
 ## 변수 입력 부분 ##
-n = int(input().strip())  # n x n 행렬
-MAP = [list(map(int, input().split())) for _ in range(n)]
-dp = [[0] * n for _ in range(n)]  # dp 테이블
-
-## dp 테이블 초기화
-dp[0][0] = 1  # 시작 위치
+n, m = map(int, input().split())  # n x m 행렬
+campus = []
+d_y, d_x = 0, 0
+for y in range(n):
+    campus.append(list(input().strip()))
+    for x in range(m):
+        # 도연이의 위치 저장
+        if campus[y][x] == "I":
+            d_y, d_x = y, x
 
 ## 문제 해결 부분 ##
-for y in range(n):
-    for x in range(n):
-        # MAP[n - 1][n - 1] 위치에서 종료
-        if MAP[y][x] == 0:
-            break
+visited = [[True] * m for _ in range(n)]
+cnt = 0
 
-        # dp 테이블의 값이 0이 아니라면 -> 문제 상황 진행
-        if dp[y][x] != 0:
-            jump = MAP[y][x]  # 점프 수
-            for dy, dx in ((0, jump), (jump, 0)):
-                new_y, new_x = y + dy, x + dx
-                # 범위가 벗어나면 -> continue
-                if new_y >= n or new_x >= n:
-                    continue
-                dp[new_y][new_x] += dp[y][x]  # 움직인 위치에 현재 위치의 dp 값을 더해간다.
 
-print(dp[n - 1][n - 1])
+def bfs(y, x):
+    global cnt
+    deq = deque()
+    deq.append((y, x))
+    visited[y][x] = False  # 도연이 방문 처리
+
+    while deq:
+        cur_y, cur_x = deq.popleft()
+        if campus[cur_y][cur_x] == "P":
+            cnt += 1
+
+        for dy, dx in ((-1, 0), (1, 0), (0, 1), (0, -1)):
+            new_y, new_x = cur_y + dy, cur_x + dx
+            # 범위를 넘거나, 이미 방문했거나, 벽이면 -> continue
+            if new_y < 0 or new_y >= n or new_x < 0 or new_x >= m or not visited[new_y][new_x] or campus[new_y][
+                new_x] == "X":
+                continue
+            visited[new_y][new_x] = False  # 방문 처리 -> 재방문 불가
+            deq.append((new_y, new_x))
+
+
+bfs(d_y, d_x)
+print(cnt if cnt != 0 else "TT")
