fe133706ddb8896cac88f2e68d3d1a6576f2d820
YongBum <dt10002@naver.com>
2023-07-03 16:33:28 +0900

    23.07.03

--- main.py
+++ main.py
@@ -3,19 +3,17 @@ from sys import stdin
 input = stdin.readline
 
 ## 변수 입력 부분 ##
-n, m = map(int, input().split())  # n x m => A 행렬
+n = int(input().strip())  # n x n 행렬
 A = [list(map(int, input().split())) for _ in range(n)]
-
-m, k = map(int, input().split())  # m x k => B 행렬
-B = [list(map(int, input().split())) for _ in range(m)]
-
-C = [[0] * k for _ in range(n)]  # (n x m) x (m x k) => (n x k) 행렬
+B = [list(map(int, input().split())) for _ in range(n)]
 
 ## 문제 해결 부분 ##
+total = 0
 for i in range(n):
-    for j in range(k):
-        for t in range(m):
-            C[i][j] += A[i][t] * B[t][j]
+    for j in range(n):
+        for t in range(n):
+            if A[i][t] == 1 and B[t][j] == 1:
+                total += 1
+                break
 
-for elem in C:
-    print(*elem, sep=" ")
+print(total)
