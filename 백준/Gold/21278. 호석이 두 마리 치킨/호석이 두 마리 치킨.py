709e8aa3a9e9cbf9fff471200576ba7eba915fd8
DT <88239689+Bumnote@users.noreply.github.com>
2023-07-04 13:15:29 +0900

    [Gold V] Title: 호석이 두 마리 치킨, Time: 196 ms, Memory: 115816 KB -BaekjoonHub

--- 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.py
+++ 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.py
@@ -24,16 +24,21 @@ for k in range(1, n + 1):
 
 res = INF
 ans = []
+# 치킨 집을 지을 노드 2개를 조합
 for y, x in combinations([v for v in range(1, n + 1)], 2):
     time = 0
+    # 1부터 순서대로 탐색하므로, 오름차순을 만족한다.
     for t in range(1, n + 1):
+        # 각 노드마다 가장 가장 치킨 집까지의 왕복 시간을 구한다.
         if t != y or t != x:
             time += min(matrix[t][y], matrix[t][x])
 
+    # 더 최단 시간이 나온다면, 왕복 최단 시간 갱신 및 배열 초기화
     if res > time:
         res = time
         ans = [(y, x, res * 2)]
+    # 최단 시간이 같은 경우는, append
     elif res == time:
         ans.append((y, x, res * 2))
 
-print(*sorted(ans)[0], sep=" ")
+print(*sorted(ans)[0], sep=" ")  # 정렬 후 가장 먼저 나오는 경우를 출력
