a738e6f99c748f6dfbe401f2a1b7327419c5fd34
YongBum <dt10002@naver.com>
2023-07-04 13:29:54 +0900

    23.07.04

--- main.py
+++ main.py
@@ -1,39 +1,44 @@
 from sys import stdin
-from collections import deque
+from itertools import combinations
 
 input = stdin.readline
 
 ## 변수 입력 부분 ##
-# (1 <= n <= 100 -> 플로이드 워셜 가능), (1 <= m <= 15), (1 <= r <= 100)
-n, m, r = map(int, input().split())  # n: 지역의 개수, m: 수색 범위, r: 길의 개수
-items = [0] + list(map(int, input().split()))  # n개의 아이템의 수들
-INF = float('inf')
-area = [[INF] * (n + 1) for _ in range(n + 1)]  # 양방향 그래프 = 대칭 행렬
+n, m = map(int, input().split())  # n: 건물의 개수, m: 도로의 개수
+INF = float("inf")
+matrix = [[INF] * (n + 1) for _ in range(n + 1)]
 
 for t in range(1, n + 1):
-    area[t][t] = 0
+    matrix[t][t] = 0
 
-# 가중치가 다르므로 인접행렬 구현
-for _ in range(r):
-    a, b, l = map(int, input().split())  # a <-> b: 양방향 그래프, 길이 l
-    area[a][b] = l
-    area[b][a] = l
+for _ in range(m):
+    a, b = map(int, input().split())  # a <-> b: 양방향 그래프 (1시간)
+    matrix[a][b] = 1
+    matrix[b][a] = 1
 
-# 플로이드 워셜 점화식 구현
 for k in range(1, n + 1):
     for i in range(1, n + 1):
         for j in range(1, n + 1):
-            if area[i][j] > area[i][k] + area[k][j]:
-                area[i][j] = area[i][k] + area[k][j]
+            if matrix[i][j] > matrix[i][k] + matrix[k][j]:
+                matrix[i][j] = matrix[i][k] + matrix[k][j]
 
-ans = 0
-for y in range(1, n + 1):
-    total = 0
-    for x in range(1, n + 1):
-        # 수색 범위 이하이면 -> 아이템 획득 가능
-        if area[y][x] <= m:
-            total += items[x]
-    if ans < total:
-        ans = total
+res = INF
+ans = []
+# 치킨 집을 지을 노드 2개를 조합
+for y, x in combinations([v for v in range(1, n + 1)], 2):
+    time = 0
+    # 1부터 순서대로 탐색하므로, 오름차순을 만족한다.
+    for t in range(1, n + 1):
+        # 각 노드마다 가장 가장 치킨 집까지의 왕복 시간을 구한다.
+        if t != y or t != x:
+            time += min(matrix[t][y], matrix[t][x])
 
-print(ans)
+    # 더 최단 시간이 나온다면, 왕복 최단 시간 갱신 및 배열 초기화
+    if res > time:
+        res = time
+        ans = [(y, x, res * 2)]
+    # 최단 시간이 같은 경우는, append
+    elif res == time:
+        ans.append((y, x, res * 2))
+
+print(*sorted(ans)[0], sep=" ")  # 정렬 후 가장 먼저 나오는 경우를 출력
