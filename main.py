d8c43758556cddc1f0f0f857a21b2cfdbe09a865
YongBum <dt10002@naver.com>
2023-07-05 09:46:25 +0900

    23.07.05

--- main.py
+++ main.py
@@ -1,35 +1,45 @@
 from sys import stdin
+from collections import deque
 
 input = stdin.readline
 
 ## 변수 입력 부분 ##
-n, k = map(int, input().split())  # n: 사건의 개수, k: 전후 관계의 개수
-INF = float('inf')
-matrix = [[INF] * (n + 1) for _ in range(n + 1)]
+n = int(input().strip())  # n: 선분의 수
+vertex = [[] for _ in range(n + 1)]
 
-for t in range(1, n + 1):
-    matrix[t][t] = 0
+lines = [0] + [tuple(map(int, input().split())) for _ in range(n)]
 
-for _ in range(k):
-    k1, k2 = map(int, input().split())  # k1 -> k2: 단방향 그래프
-    matrix[k1][k2] = 1
+for i in range(1, n):
+    for j in range(i + 1, n + 1):
+        if lines[j][1] < lines[i][0] or lines[i][1] < lines[j][0]:
+            continue
+        # 가중치 = 1
+        vertex[i].append(j)
+        vertex[j].append(i)
 
-# 플로이드 워셜 점화실 구현
-for k in range(1, n + 1):
-    for i in range(1, n + 1):
-        for j in range(1, n + 1):
-            if matrix[i][j] > matrix[i][k] + matrix[k][j]:
-                matrix[i][j] = matrix[i][k] + matrix[k][j]
+q = int(input().strip())
 
-s = int(input().strip())  # s: 사건 쌍의 수
-for _ in range(s):
-    s1, s2 = map(int, input().split())  # (s1, s2)
-    # 두 사건의 전후관계를 모르는 경우: 0
-    if matrix[s1][s2] == INF and matrix[s2][s1] == INF:
-        print(0)
-    # s2 -> s1 관계인 경우: 1
-    elif matrix[s1][s2] == INF:
-        print(1)
-    # s1 -> s2 관계인 경우: -1
-    else:
-        print(-1)
+
+def bfs(start, end):
+    deq = deque()
+    deq.append((start, 0))
+    visited = [True] * (n + 1)
+    visited[start] = False  # 방문 처리
+    while deq:
+        cur_x, cur_cnt = deq.popleft()
+        if cur_x == end:
+            return cur_cnt
+
+        for nxt in vertex[cur_x]:
+            # 방문이 가능하다면 -> append
+            if visited[nxt]:
+                visited[nxt] = False  # 방문 처리
+                deq.append((nxt, cur_cnt + 1))
+
+    return -1
+
+
+# q: 질문의 수
+for _ in range(q):
+    A, B = map(int, input().split())  # 두 선분의 번호
+    print(bfs(A, B))
