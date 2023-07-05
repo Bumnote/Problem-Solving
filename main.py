7c2e4170d95564215f7dd4d6c9613cfb6e4a1c0b
YongBum <dt10002@naver.com>
2023-07-05 23:39:50 +0900

    23.07.05

--- main.py
+++ main.py
@@ -1,45 +1,41 @@
 from sys import stdin
-from collections import deque
 
 input = stdin.readline
 
 ## 변수 입력 부분 ##
-n = int(input().strip())  # n: 선분의 수
-vertex = [[] for _ in range(n + 1)]
+n, m = map(int, input().split())  # n: 도시의 개수, m: 도로의 개수
+INF = float('inf')
+matrix = [[INF] * (n + 1) for _ in range(n + 1)]
+for _ in range(m):
+    A, B, T = map(int, input().split())  # A -> B: T 시간
+    matrix[A][B] = T  # 단방향 인접행렬
 
-lines = [0] + [tuple(map(int, input().split())) for _ in range(n)]
+for t in range(1, n + 1):
+    matrix[t][t] = 0
 
-for i in range(1, n):
-    for j in range(i + 1, n + 1):
-        if lines[j][1] < lines[i][0] or lines[i][1] < lines[j][0]:
-            continue
-        # 가중치 = 1
-        vertex[i].append(j)
-        vertex[j].append(i)
+total = int(input().strip())  # 준형이와 친구들의 총 인원
+citys = list(map(int, input().split()))  # k명의 도시 번호
 
-q = int(input().strip())
+## 문제 해결 부분 ##
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if matrix[i][j] > matrix[i][k] + matrix[k][j]:
+                matrix[i][j] = matrix[i][k] + matrix[k][j]
 
+ans = INF
+lst = []
+for v in range(1, n + 1):
+    temp = 0
+    for city in citys:
+        # 왕복 거리의 최댓값의 최소를 가지는 도시만을 구한다.
+        temp = max(temp, matrix[city][v] + matrix[v][city])
+    # 왕복 거리의 최댓값의 최소라면 -> 그 도시가 정답이다.
+    if ans > temp:
+        ans = temp
+        lst = [v]
+    # 왕복 거리의 최댓값의 최솟값이 같다면 -> append
+    elif ans == temp:
+        lst.append(v)
 
-def bfs(start, end):
-    deq = deque()
-    deq.append((start, 0))
-    visited = [True] * (n + 1)
-    visited[start] = False  # 방문 처리
-    while deq:
-        cur_x, cur_cnt = deq.popleft()
-        if cur_x == end:
-            return cur_cnt
-
-        for nxt in vertex[cur_x]:
-            # 방문이 가능하다면 -> append
-            if visited[nxt]:
-                visited[nxt] = False  # 방문 처리
-                deq.append((nxt, cur_cnt + 1))
-
-    return -1
-
-
-# q: 질문의 수
-for _ in range(q):
-    A, B = map(int, input().split())  # 두 선분의 번호
-    print(bfs(A, B))
+print(*lst, sep=" ")
