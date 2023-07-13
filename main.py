f660e72f9ff6b4a97ec08d74121e554f7aaf21b7
YongBum <dt10002@naver.com>
2023-07-13 10:38:37 +0900

    23.07.13

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from sys import stdin
+from math import isqrt
 
 input = stdin.readline
 
 ## 변수 입력 부분 ##
-n = int(input().strip())
+MIN, MAX = map(int, input().split())  # MIN <= X <= MAX
 
-## 문제 풀이 부분 ##
-print("SK" if n % 2 == 1 else "CY")
+## 문제 해결 부분 ##
+n_list = [1] * (MAX - MIN + 1)
+
+for i in range(2, isqrt(MAX) + 1):
+    k = i ** 2
+    pos = 0 if MIN % k == 0 else k - (MIN % k)
+    for j in range(pos, len(n_list), k):
+        n_list[j] = 0
+
+print(sum(n_list))
