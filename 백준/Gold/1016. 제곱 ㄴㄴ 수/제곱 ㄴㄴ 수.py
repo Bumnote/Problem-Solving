8546d4e6fec91f12755263c0f4b8fca8b158a17f
DT <88239689+Bumnote@users.noreply.github.com>
2023-07-13 10:36:18 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 168 ms, Memory: 123084 KB -BaekjoonHub

---
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.py
@@ -0,0 +1,17 @@
+from sys import stdin
+from math import isqrt
+
+input = stdin.readline
+
+## 변수 입력 부분 ##
+MIN, MAX = map(int, input().split())  # MIN <= X <= MAX
+
+n_list = [1] * (MAX - MIN + 1)
+
+for i in range(2, isqrt(MAX) + 1):
+    k = i ** 2
+    pos = 0 if MIN % k == 0 else k - (MIN % k)
+    for j in range(pos, len(n_list), k):
+        n_list[j] = 0
+
+print(sum(n_list))
