3adc28eb6ef740e9cfa9dd90f91e2dc54ea76037
YongBum <dt10002@naver.com>
2023-07-15 23:36:58 +0900

    23.07.15

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
 from sys import stdin
-from math import isqrt
+from math import comb
 
 input = stdin.readline
 
 ## 변수 입력 부분 ##
-MIN, MAX = map(int, input().split())  # MIN <= X <= MAX
+n, m = map(int, input().split())  # nCm
 
-## 문제 해결 부분 ##
-n_list = [1] * (MAX - MIN + 1)
-
-for i in range(2, isqrt(MAX) + 1):
-    k = i ** 2
-    pos = 0 if MIN % k == 0 else k - (MIN % k)
-    for j in range(pos, len(n_list), k):
-        n_list[j] = 0
-
-print(sum(n_list))
+## 문제 풀이 부분 ##
+print(comb(n, m))
