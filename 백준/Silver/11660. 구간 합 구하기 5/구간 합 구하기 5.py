6d70d071db3b6539ee27dcfabdbf537a0f7a6718
DT <88239689+Bumnote@users.noreply.github.com>
2023-07-17 17:08:19 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 424 ms, Memory: 134116 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -4,16 +4,19 @@ input = stdin.readline
 
 n, m = map(int, input().split())  # n x n 행렬, m: 테스트 케이스
 matrix = [list(map(int, input().split())) for _ in range(n)]
-pre = [[0] * (n + 1) for _ in range(n)]
+dp = [[0] * (n + 1) for _ in range(n + 1)]
 
-for y in range(n):
-    for x in range(1, n + 1):
-        pre[y][x] = pre[y][x - 1] + matrix[y][x - 1]
+# dp 초기화
+dp[1][1] = matrix[0][0]
+for t in range(2, n + 1):
+    dp[1][t] = dp[1][t - 1] + matrix[0][t - 1]
+    dp[t][1] = dp[t - 1][1] + matrix[t - 1][0]
+
+for i in range(2, n + 1):
+    for j in range(2, n + 1):
+        dp[i][j] = matrix[i - 1][j - 1] + dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1]
 
 for _ in range(m):
-    x1, y1, x2, y2 = map(int, input().split())  # (x1, y1), (x2, y2)
-    ans = 0
-    for y in range(x1 - 1, x2):
-        ans += pre[y][y2] - pre[y][y1 - 1]
-
+    y1, x1, y2, x2 = map(int, input().split())
+    ans = dp[y2][x2] - dp[y2][x1 - 1] - dp[y1 - 1][x2] + dp[y1 - 1][x1 - 1]
     print(ans)
