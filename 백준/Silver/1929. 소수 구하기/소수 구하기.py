798de57246ce227088932171680ea25c71a64cac
DT <88239689+Bumnote@users.noreply.github.com>
2023-07-18 10:23:16 +0900

    [Silver III] Title: 소수 구하기, Time: 204 ms, Memory: 131584 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,13 +1,17 @@
 from sys import stdin
+from math import isqrt
 
 input = stdin.readline
 
 M, N = map(int, input().split())  # M이상 N이하의 소수
 
 sieve = [False, False] + [True] * N
-for i in range(2, N + 1):
+for i in range(2, isqrt(N) + 1):
     if sieve[i]:
-        if M <= i <= N:
-            print(i)
         for j in range(i * 2, N + 1, i):
             sieve[j] = False  # 소수가 아님을 표시
+
+# M이상 N이하의 소수인 것만 출력 
+for i in range(M, N + 1):
+    if sieve[i]:
+        print(i)
