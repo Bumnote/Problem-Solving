e11974d82beec9c04aa38e1cd392ac5046366a1c
YongBum <dt10002@naver.com>
2023-07-19 18:01:35 +0900

    23.07.19

--- note.py
+++ note.py
@@ -2,3 +2,19 @@ from sys import stdin
 
 input = stdin.readline
 
+n = int(input().strip())  # 1 <= n <= 8
+n_list = []
+
+
+def backtrack(cur_cnt):
+    if cur_cnt > n:
+        print(*n_list)
+        return
+
+    for i in range(1, n + 1):
+        n_list.append(i)
+        backtrack(cur_cnt + 1)
+        n_list.pop()
+
+
+backtrack(1)
