4db178dadacf0228882c297c0b618f242e1f313f
DT <88239689+Bumnote@users.noreply.github.com>
2023-07-20 15:59:28 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 124 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.py
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.py
@@ -3,5 +3,4 @@ from sys import stdin
 input = stdin.readline
 
 N, M = map(int, input().split())  # N x M 크기의 초콜릿
-ans = (min(N, M) - 1) + (min(N, M) * (max(N, M) - 1))
-print(ans)
+print(N * M - 1)
