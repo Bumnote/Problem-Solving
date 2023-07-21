79626b7802dd9dae6e3b533a3d00b0beae5369a1
YongBum <dt10002@naver.com>
2023-07-21 17:19:37 +0900

    23.07.21

--- note.py
+++ note.py
@@ -3,34 +3,19 @@ from math import isqrt
 
 input = stdin.readline
 
-tc = int(input().strip())  # tc: 테스트 케이스
-# 에라토스테네스의 체 구현
-sieve = [False, False] + [True] * 100_000
-primes = []
-for i in range(2, 100_000):
-    if sieve[i]:
-        primes.append(i)  # 소수만 따로 리스트에 담는다.
-        for j in range(2 * i, 100_000 + 1, i):
-            sieve[j] = False  # 소수가 아님을 처리
+n = int(input().strip())  # n 이하의 n과 서로소인 자연수를 구하자.
+ans = n
 
-for elem in range(2, 100_000):
-    if sieve[elem]:
-        print(elem)
-for _ in range(tc):
-    n = int(input().strip())  # n: 양의 정수 (2 <= n <= 100_000)
-    # n이 소수라면 -> 자기 자신이 인수 그 자체이다.
-    if sieve[n]:
-        print(f"{n} {1}")
-    else:
-        for prime in primes:
-            # 해당 소수로 나눠질때까지 나눈다.
-            cnt = 0
-            while n % prime == 0:
-                n //= prime
-                cnt += 1  # 나눈 횟수를 더해간다.
+# 해당 수의 제곱근까지만 탐색한다.
+for i in range(2, isqrt(n) + 1):
+    if n % i == 0:
+        ans -= ans // i  # i를 약수로 가지는 수들 제거
+        while n % i == 0:
+            n //= i  # 숫자 n의 소인수에서 i를 모두 제거
 
-            if cnt != 0:
-                print(f"{prime} {cnt}")
-            # 모두 다 나눠졌다는 의미이므로 결과 출력
-            if n == 1:
-                break
+# 제곱근보다 큰 소인수가 존재하는 경우 -> 제거
+# 제곱근보다 큰 소인수가 있다면, 반드시 하나만 존재한다.
+if n > 1:
+    ans -= ans // n
+
+print(ans)
