ba4d3e9c80e25dd0d095414fdcfdbf961edae636
DT <88239689+Bumnote@users.noreply.github.com>
2023-07-21 12:04:31 +0900

    [Gold V] Title: 거의 소수, Time: 2268 ms, Memory: 359248 KB -BaekjoonHub

--- 백준/Gold/1456. 거의 소수/거의 소수.py
+++ 백준/Gold/1456. 거의 소수/거의 소수.py
@@ -3,23 +3,23 @@ from math import isqrt
 
 input = stdin.readline
 
-MAX_LENGTH = 10_000_000
-sieve = [False, False] + [True] * (MAX_LENGTH - 1)
-primes = []
-for i in range(2, MAX_LENGTH + 1):
-    if sieve[i]:
-        primes.append(i)
-        for j in range(2 * i, MAX_LENGTH + 1, i):
-            sieve[j] = False  # 소수가 아님을 처리
-
 A, B = map(int, input().split())  # A <= 거의 소수 <= B (1 <= A <= B <= 10^14)
 
+MAX_LENGTH = isqrt(B)
+sieve = [False, False] + [True] * (MAX_LENGTH - 1)
+
 ans = 0
-for prime in primes:
-    n = 2
-    while prime ** n <= B:
-        if A <= prime ** n:
-            ans += 1
-        n += 1
+for i in range(2, MAX_LENGTH + 1):
+    if sieve[i]:
+        temp = i
+        n = 2
+        # 큰 수를 작은 수로 표현하기 위해서 이항 정리 활용
+        while i <= B / temp:
+            if A / temp <= i:
+                ans += 1
+            temp *= i
+
+        for j in range(2 * i, MAX_LENGTH + 1, i):
+            sieve[j] = False  # 소수가 아님을 처리
 
 print(ans)
