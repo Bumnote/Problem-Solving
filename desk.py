9232674f7eb49231a20f680d9d1ea2e980bb7990
KIM_DESKTOP <dt10002@naver.com>
2023-07-22 23:42:49 +0900

    23.07.22

--- desk.py
+++ desk.py
@@ -2,15 +2,19 @@ from sys import stdin
 
 input = stdin.readline
 
-n = int(input().strip())  # n번째 소수
-sieve = [False, False] + [True] * 110_000
-cnt = 0
-for i in range(2, len(sieve)):
-    if sieve[i]:
-        cnt += 1
-        # n번째 소수인 경우 -> 출력 후 break
-        if cnt == n:
-            print(i)
-            break
-        for j in range(2 * i, len(sieve), i):
-            sieve[j] = False  # 소수가 아님을 처리
+
+def get_gcd(a, b):
+    if a < b:
+        a, b = b, a
+
+    if b == 0:
+        return a
+
+    return get_gcd(b, a % b)
+
+
+tc = int(input().strip())  # tc: 테스트 케이스
+
+for _ in range(tc):
+    a, b = map(int, input().split())  # a, b의 최대 공약수를 구하자.
+    print(get_gcd(a, b))  # 유클리드 호제법 구현
