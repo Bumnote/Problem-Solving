1dfab92ca0453862ac3e3fa6883c97561f75f899
KIM_DESKTOP <dt10002@naver.com>
2023-07-24 23:46:32 +0900

    23.07.24

--- desk.py
+++ desk.py
@@ -1,23 +1,29 @@
 from sys import stdin
+from heapq import heappush, heappop
 
 input = stdin.readline
 
-n, s = map(int, input().split())  # n: 동생 수, s:수빈 위치
-pos_l = list(map(lambda x: abs(s - x), map(int, input().split())))
+N, M, K = map(int, input().split())  # N: 기간, M: 선호도 합, K: 맥주의 종류
+pq = []
+for _ in range(K):
+    v, c = map(int, input().split())  # v: 선호도, c: 도수 레벨
+    heappush(pq, (c, -v))  # 도수 레벨은 오름차순, 선호도는 내림차순
 
+flavor = 0  # 선호도의 합
+lst = []  # 선호도를 우선순위 큐에 담는다.
+flag = False
+while pq:
+    c, v = heappop(pq)
+    heappush(lst, -v)  # 도수 레벨이 작으면서, 선호도의 합이 큰 것부터 담는다.
+    flavor -= v  # 선호도를 더해간다.
+    if len(lst) == N:
+        if flavor >= M:
+            flag = True
+            break
+        else:
+            flavor -= heappop(lst)  # 선호도가 가장 낮은 것을 pop한다.
 
-def get_gcd(a, b):
-    if a < b:
-        a, b = b, a
-
-    if b == 0:
-        return a
-
-    return get_gcd(b, a % b)
-
-
-ans = pos_l[0]
-for num in pos_l:
-    ans = get_gcd(ans, num)
-
-print(ans)
+if flag:
+    print(c)
+else:
+    print(-1)
