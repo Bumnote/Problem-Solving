b7d7c71629811d44a63dda6c7b7e881ec0ef0de4
YongBum <dt10002@naver.com>
2023-07-25 13:23:03 +0900

    23.07.25

--- note.py
+++ note.py
@@ -3,27 +3,37 @@ from heapq import heappush, heappop
 
 input = stdin.readline
 
-N, M, K = map(int, input().split())  # N: 기간, M: 선호도 합, K: 맥주의 종류
-pq = []
-for _ in range(K):
-    v, c = map(int, input().split())  # v: 선호도, c: 도수
-    heappush(pq, (c, -v))  # 도수는 낮은 순서, 선호도는 높은 순서
+tc = int(input().strip())  # tc: 테스트 케이스
 
-flavor = []
-flag = False
-while pq:
-    c, v = heappop(pq)
-    print(f"c = {c} / v = {v}")
-    flavor.append(-v)
-    if len(flavor) == N:
-        if sum(flavor) >= M:
-            flag = True
-            break
+for _ in range(tc):
+    min_pq = []  # 최소 힙
+    max_pq = []  # 최대 힙
+    k = int(input().strip())  # k: 입력값의 개수
+    visited = [1] * k  # 방문 가능 판별 리스트
+    for i in range(k):
+        command, num = input().split()
+        num = int(num)
+        # 삽입하는 경우
+        if command == "I":
+            heappush(min_pq, (num, i))
+            heappush(max_pq, (-num, i))
+            visited[i] = True  # 방문 가능 처리
+        # 삭제하는 경우
         else:
-            # 선호도가 가장 낮은 것을 pop
-            heappop(flavor)
+            # 최댓값을 삭제하는 경우
+            if num == 1:
+                if max_pq:
+                    visited[heappop(max_pq)[1]] = 0  # 방문 불가능 처리
+            # 최솟값을 삭제하는 경우
+            else:
+                if min_pq:
+                    visited[heappop(min_pq)[1]] = 0  # 방문 불가능 처리
+        while max_pq and visited[max_pq[0][1]] == 0:
+            heappop(max_pq)
+        while min_pq and visited[min_pq[0][1]] == 0:
+            heappop(min_pq)
 
-if flag:
-    print(c)
-else:
-    print(-1)
+    if min_pq and max_pq:
+        print(f"{-max_pq[0][0]} {min_pq[0][0]}")
+    else:
+        print("EMPTY")
