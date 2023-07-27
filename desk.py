f5dc6134472a4f0fa082b836112459145d97070d
KIM_DESKTOP <dt10002@naver.com>
2023-07-27 23:42:56 +0900

    23.07.27

--- desk.py
+++ desk.py
@@ -2,8 +2,20 @@ from sys import stdin
 
 input = stdin.readline
 
-N = int(input().strip())  # N: A 원소의 개수
-A = list(map(int, input().split()))
+n = int(input().strip())  # n: 숫자의 개수 (1 <= n <= 1_000)
+n_list = list(map(int, input().split()))  # n개의 숫자
 
-M = int(input().strip())  # M: B 원소의 개수
-B = list(map(int, input().split()))
+# R = N - Q x D (Q: 몫, R: 나머지)
+def get_gcd(a, b):
+    if a < b:
+        a, b = b, a
+
+    if b == 0:
+        return a
+    return get_gcd(b, a % b)
+
+gcd = n_list[0]
+for i in n_list:
+    gcd = get_gcd(gcd, i)
+
+print(gcd)
