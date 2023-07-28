e72196f450cd6fabb7f3647a075dcc41270f610f
YongBum <dt10002@naver.com>
2023-07-28 17:56:02 +0900

    23.07.28

--- note.py
+++ note.py
@@ -1,40 +1,64 @@
 from sys import stdin
+from collections import deque
 
 input = stdin.readline
 
+R, C = map(int, input().split())
+MAP = []
+water = deque()
+me = deque()
+for y in range(R):
+    MAP.append(list(input().strip()))
+    for x in range(C):
+        if MAP[y][x] == "S":
+            me.append((y, x, 0))  # 내 위치 저장
+        elif MAP[y][x] == "*":
+            water.append((y, x, 0))  # 물의 위치 저장
 
-def get_time(Z):
-    time = 1 + (X - Z) / V_list[N]
-    return time
+
+# .: 비어있는 곳, *: 물, X: 돌, D: 굴, S: 고슴도치 위치
+# S -> D
 
 
-T = int(input().strip())  # T: 테스트 케이스
+def bfs():
+    # 물의 이동 구현
+    while water:
+        cur_y, cur_x, cur_cnt = water.popleft()
 
-for _ in range(T):
-    N, X, Y = map(int, input().split())  # N: 사람의 수, X: 트랙의 길이, Y: 부스터 한계치
-    V_list = [0] + list(map(int, input().split()))  # N개의 정수 -> 각 자동차의 속도 V[i]
-    times = [0] + [X / V_list[i] for i in range(1, N + 1)]
-    MIN_TIME = min(times[1:])
-    # 부스터를 쓰지 않고도 단독 우승이 가능한 경우
-    if times[N] == MIN_TIME and times[1:].count(MIN_TIME) == 1:
-        print(0)
-        continue
-    # 나는 N번 선수
-    # i번 참가자의 속도 - V[i] / 나는 처음 1초간 Z m/s 로 움직일 수 있다. (Z <= Y)
-    left, right = 0, Y
-    Z = Y  # 정답 변수
-    while left <= right:
-        mid = (left + right) // 2
+        visited_w[cur_y][cur_x] = cur_cnt
 
-        if MIN_TIME > get_time(mid):
-            right = mid - 1
-            Z = min(Z, mid)
-        else:
-            left = mid + 1
+        for ny, nx in ((0, 1), (0, -1), (1, 0), (-1, 0)):
+            new_y, new_x = cur_y + ny, cur_x + nx
+            # 범위를 넘지 않고, 방문한 적이 없다면 -> append()
+            if new_y < 0 or new_y >= R or new_x < 0 or new_x >= C:
+                continue
 
-    # 부스터를 최대치로 사용하고도 단독 우승이 불가능한 경우
-    if Z == Y:
-        print(-1)
-    # 최적의 부스터를 찾아낸 경우
-    else:
-        print(Z)
+            if visited_w[new_y][new_x] == -1 and MAP[new_y][new_x] == ".":
+                water.append((new_y, new_x, cur_cnt + 1))
+
+    # 고슴도치의 이동 구현 -> 물보다 빨리 움직이면서 굴에 가야만 한다.
+    while me:
+        cur_y, cur_x, cur_cnt = me.popleft()
+
+        if MAP[cur_y][cur_x] == "D":
+            return cur_cnt
+
+        visited_m[cur_y][cur_x] = cur_cnt
+
+        for ny, nx in ((0, 1), (0, -1), (1, 0), (-1, 0)):
+            new_y, new_x = cur_y + ny, cur_x + nx
+            if new_y < 0 or new_y >= R or new_x < 0 or new_x >= C:
+                continue
+
+            if visited_m[new_y][new_x] == -1 and MAP[new_y][new_x] != "X" and \
+                    (visited_w[new_y][new_x] > cur_cnt + 1 or MAP[new_y][new_x] == "D"):
+                me.append((new_y, new_x, cur_cnt + 1))
+
+    return "KAKTUS"
+
+
+# 물 먼저 이동
+visited_w = [[-1] * C for _ in range(R)]
+visited_m = [[-1] * C for _ in range(R)]
+
+print(bfs())
