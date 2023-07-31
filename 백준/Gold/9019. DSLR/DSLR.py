969f294431df6c82017daf73a83db400490c2dca
DT <88239689+Bumnote@users.noreply.github.com>
2023-07-31 16:16:18 +0900

    [Gold IV] Title: DSLR, Time: 17360 ms, Memory: 228568 KB -BaekjoonHub

---
+++ 백준/Gold/9019. DSLR/DSLR.py
@@ -0,0 +1,49 @@
+from sys import stdin
+from collections import deque
+
+input = stdin.readline
+
+T = int(input().strip())  # T: 테스트 케이스
+
+
+def bfs(A, B):
+    visited = [-1] * 10_000
+    command = [""] * 10_000
+    dq = deque()
+    dq.append(A)
+    visited[A] = "*"
+
+    while dq:
+        cur_A = dq.popleft()
+
+        #  레지스터의 초기값이 주어진 최종값이 되었다면 -> 과정 출력
+        if cur_A == B:
+            ans = []
+            while True:
+                if cur_A == A:
+                    # 과정을 역순으로 출력 -> return
+                    print(*ans[::-1], sep="")
+                    return
+                ans.append(command[cur_A])  # 이전의 사용한 명령어들을 저장
+                cur_A = visited[cur_A]  # 현재 값을 이전 값으로 변경ㄴ
+
+        D_A = (cur_A * 2) % 10_000 if cur_A * 2 > 9999 else cur_A * 2
+        S_A = 9999 if cur_A == 0 else cur_A - 1
+        STR_A = "0" * (4 - len(str(cur_A))) + str(cur_A)  # 부족한 0의 개수를 채워준다.
+        L_A = int(STR_A[1:] + STR_A[0])
+        R_A = int(STR_A[-1] + STR_A[:-1])
+
+        for nxt, c in ((D_A, "D"), (S_A, "S"), (L_A, "L"), (R_A, "R")):
+            # 범위를 넘지 않고, 방문한 적이 없다면 -> 다음 숫자로 이동
+            if 0 <= nxt <= 10_000 and visited[nxt] == -1:
+                visited[nxt] = cur_A  # 다음 숫자 위치에 현재 숫자를 저장
+                command[nxt] = c  # 다음 숫자의 사용한 커맨드를 입력
+                dq.append(nxt)
+
+    return
+
+
+for _ in range(T):
+    A, B = map(int, input().split())
+
+    bfs(A, B)
