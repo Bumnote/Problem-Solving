05de2225faae659ac48706cef056510c6e28481b
YongBum <dt10002@naver.com>
2023-08-02 17:05:03 +0900

    23.08.02

--- note.py
+++ note.py
@@ -1,32 +1,49 @@
-from sys import stdin, setrecursionlimit
+from sys import stdin
 
-setrecursionlimit(10_000)
 input = stdin.readline
 
-N, M = map(int, input().split())  # N x M MAP
-MAP = [list(map(int, input().split())) for _ in range(N)]
-visited = [[True] * M for _ in range(N)]
-visited[0][0] = False  # 출발점 방문 표시
-cnt = 0
+
+def dfs(node):
+    global v_cnt, e_cnt
+
+    for nxt in vertex[node]:
+        # 다음 노드로 방문이 가능하다면 -> dfs 탐색
+        if visited[nxt]:
+            v_cnt += 1  # 정점의 개수 증가
+            e_cnt += len(vertex[nxt])  # 간선의 개수 증가
+            visited[nxt] = False  # 방문 처리
+            dfs(nxt)
 
 
-def dfs(y, x):
-    global cnt
+tc = 1
+while True:
+    n, m = map(int, input().split())  # n: 정점 수, m: 간선 수
+    if n == 0 and m == 0:
+        break
 
-    if y == N - 1 and x == M - 1:
-        cnt += 1
+    vertex = [[] for _ in range(n + 1)]
+    visited = [True] * (n + 1)
+    for _ in range(m):
+        s, e = map(int, input().split())  # s <-> e: 양방향 그래프
+        vertex[s].append(e)
+        vertex[e].append(s)
 
-    for dy, dx in ((0, 1), (0, -1), (1, 0), (-1, 0)):
-        new_y, new_x = y + dy, x + dx
-        # 범위를 넘는 경우 -> 탐색하지 않는다.
-        if new_y < 0 or new_y >= N or new_x < 0 or new_x >= M:
-            continue
-        # 방문 가능하고, 현재 위치보다 낮은 지역이라면 -> 탐색을 시도한다.
-        if visited[new_y][new_x] and MAP[y][x] > MAP[new_y][new_x]:
-            visited[new_y][new_x] = False  # 방문 표시
-            dfs(new_y, new_x)
-            visited[new_y][new_x] = True  # 복원 표시
+    cnt = 0
+    for i in range(1, n + 1):
+        # 아직 방문한 적이 없는 노드라면 -> dfs 탐색
+        if visited[i]:
+            v_cnt, e_cnt = 1, len(vertex[i])  # 정점의 개수, 간선의 개수
+            visited[i] = False  # 방문 처리
+            dfs(i)
+            # 트리의 성질을 만족한다면 -> 트리의 개수 증가
+            if (v_cnt - 1) * 2 == e_cnt:
+                cnt += 1
 
+    if cnt == 0:
+        print(f"Case {tc}: No trees.")
+    elif cnt == 1:
+        print(f"Case {tc}: There is one tree.")
+    else:
+        print(f"Case {tc}: A forest of {cnt} trees.")
 
-dfs(0, 0)
-print(cnt)
+    tc += 1  # 테스트 케이스 증가
