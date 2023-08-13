25f8de7f12220055f6a07581b8cc8e74cab247ed
KIM_DESKTOP <dt10002@naver.com>
2023-08-14 00:14:05 +0900

    23.08.14

--- desk.py
+++ desk.py
@@ -1,25 +1,20 @@
 from sys import stdin
-from math import isqrt
-from itertools import combinations
 
 input = stdin.readline
 
-n, m = map(int, input().split())  # n: 소들의 수, m: 선별할 소의 수
-H_i = list(map(int, input().split()))
+total = 0
+n_list = [int(input().strip()) for _ in range(8)]
 
-MAX_NUM = 9_000
-sieve = [False, False] + [True] * (MAX_NUM - 1)
-for i in range(2, isqrt(MAX_NUM) + 1):
-    if sieve[i]:
-        for j in range(2 * i, MAX_NUM, i):
-            sieve[j] = False  # 소수가 아님 처리
+ans = []
+for i, n in enumerate(n_list, start=1):
+    ans.append((n, i))
 
-ans = set()
-for elem in combinations(H_i, m):
-    if sieve[sum(elem)]:
-        ans.add(sum(elem))
+ans = (sorted(ans, reverse=True)[:5])
+ans.sort(key=lambda x: x[1])
+nums = []
+for elem, index in ans:
+    total += elem
+    nums.append(index)
 
-if len(ans) == 0:
-    print(-1)
-else:
-    print(*sorted(list(ans)))
+print(total)
+print(*nums)
