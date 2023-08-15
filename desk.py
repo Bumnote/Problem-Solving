a67ca014f622e67dd6aaaa68ee92a7a2a1e2ecd7
KIM_DESKTOP <dt10002@naver.com>
2023-08-15 10:53:14 +0900

    23.08.15

--- desk.py
+++ desk.py
@@ -1,20 +1,44 @@
-from sys import stdin
+from sys import stdin, maxsize
+from heapq import heappush, heappop
 
 input = stdin.readline
+INF = maxsize
 
-total = 0
-n_list = [int(input().strip()) for _ in range(8)]
+n, v, e = map(int, input().split())  # n: 팀원의 수, v: 장소의 수, e: 도로의 수
+a, b = map(int, input().split())  # a: KIST의 위치, b: 씨알푸드의 위치
+H = list(map(int, input().split()))  # n명의 집의 위치
 
-ans = []
-for i, n in enumerate(n_list, start=1):
-    ans.append((n, i))
+vertex = [[] for _ in range(v + 1)]
+for _ in range(e):
+    s, t, l = map(int, input().split())  # s <-> t:가중치 l 양방향
+    vertex[s].append((t, l))
+    vertex[t].append((s, l))
 
-ans = (sorted(ans, reverse=True)[:5])
-ans.sort(key=lambda x: x[1])
-nums = []
-for elem, index in ans:
-    total += elem
-    nums.append(index)
 
-print(total)
-print(*nums)
+# 다익스트라 구현 함수 -> 출발지로에서 시작하여 모든 정점까지의 최단거리를 구한다.
+def dijkstra(s):
+    dist = [INF] * (v + 1)
+    dist[s] = 0
+    pq = [(0, s)]
+
+    while pq:
+        min_dist, cur_v = heappop(pq)
+
+        if dist[cur_v] != min_dist:
+            continue
+
+        for nxt_v, nxt_dist in vertex[cur_v]:
+            new_dist = dist[cur_v] + nxt_dist
+            if new_dist < dist[nxt_v]:
+                dist[nxt_v] = new_dist
+                heappush(pq, (new_dist, nxt_v))
+
+    # KIST 또는 씨알푸드까지 가지 못하는 경우의 거리는 -1
+    return (dist[a] if dist[a] != INF else -1) + (dist[b] if dist[b] != INF else - 1)
+
+
+ans = 0
+for house in H:
+    ans += dijkstra(house)
+
+print(ans)
