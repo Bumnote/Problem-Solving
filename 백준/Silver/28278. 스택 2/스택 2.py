c44eddbf6aef6dc6bacea276dc38d0036e72d8f3
DT <88239689+Bumnote@users.noreply.github.com>
2023-08-16 11:57:23 +0900

    [Silver IV] Title: 스택 2, Time: 504 ms, Memory: 223216 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -0,0 +1,33 @@
+from sys import stdin
+
+input = stdin.readline
+
+n = int(input().strip())  # n: 명령의 수
+stk = []
+
+for _ in range(n):
+    command = input().split()
+    if len(command) == 2:
+        c, num = command
+        stk.append(num)
+
+    elif command[0] == "2":
+        if stk:
+            print(stk.pop())
+        else:
+            print(-1)
+
+    elif command[0] == "3":
+        print(len(stk))
+
+    elif command[0] == "4":
+        if stk:
+            print(0)
+        else:
+            print(1)
+            
+    elif command[0] == "5":
+        if stk:
+            print(stk[-1])
+        else:
+            print(-1)
