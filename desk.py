1543446b6257d9ffb021afd3cdf414ed71dcf7c1
KIM_DESKTOP <dt10002@naver.com>
2023-08-19 00:46:02 +0900

    23.08.19

--- desk.py
+++ desk.py
@@ -1,44 +1,40 @@
 from sys import stdin, maxsize
-from heapq import heappush, heappop
 
 input = stdin.readline
 INF = maxsize
 
-n, v, e = map(int, input().split())  # n: 팀원의 수, v: 장소의 수, e: 도로의 수
-a, b = map(int, input().split())  # a: KIST의 위치, b: 씨알푸드의 위치
-H = list(map(int, input().split()))  # n명의 집의 위치
+n, m = map(int, input().split())  # n: 집하장의 개수, m: 경로의 개수
+mat = [[INF] * (n + 1) for _ in range(n + 1)]
+ans = [[0] * (n + 1) for _ in range(n + 1)]
 
-vertex = [[] for _ in range(v + 1)]
-for _ in range(e):
-    s, t, l = map(int, input().split())  # s <-> t:가중치 l 양방향
-    vertex[s].append((t, l))
-    vertex[t].append((s, l))
+for i in range(1, n + 1):
+    mat[i][i] = 0
+    ans[i][i] = "-"
 
+for _ in range(m):
+    a, b, t = map(int, input().split())  # a <-> b:시간 t 양방향
+    mat[a][b], mat[b][a] = t, t
+    ans[a][b], ans[b][a] = b, a
 
-# 다익스트라 구현 함수 -> 출발지로에서 시작하여 모든 정점까지의 최단거리를 구한다.
-def dijkstra(s):
-    dist = [INF] * (v + 1)
-    dist[s] = 0
-    pq = [(0, s)]
+# 정답 출력
+for y in range(1, n + 1):
+    for x in range(1, n + 1):
+        print(ans[y][x], end=" ")
+    print()
 
-    while pq:
-        min_dist, cur_v = heappop(pq)
+print()
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if mat[i][j] > mat[i][k] + mat[k][j]:
+                mat[i][j] = mat[i][k] + mat[k][j]
+                # i -> j보다 i -> k -> j가 더 빠르다면 -> [i][j] 위치에 [i][k] 값 저장
+                ans[i][j] = ans[i][k]
+                print(f"{i} -> {k} -> {j}")
+                print(f"ans[{i}][{j}] = {ans[i][j]} / ans[{i}][{k}] = {ans[i][k]}")
 
-        if dist[cur_v] != min_dist:
-            continue
-
-        for nxt_v, nxt_dist in vertex[cur_v]:
-            new_dist = dist[cur_v] + nxt_dist
-            if new_dist < dist[nxt_v]:
-                dist[nxt_v] = new_dist
-                heappush(pq, (new_dist, nxt_v))
-
-    # KIST 또는 씨알푸드까지 가지 못하는 경우의 거리는 -1
-    return (dist[a] if dist[a] != INF else -1) + (dist[b] if dist[b] != INF else - 1)
-
-
-ans = 0
-for house in H:
-    ans += dijkstra(house)
-
-print(ans)
+# 정답 출력
+for y in range(1, n + 1):
+    for x in range(1, n + 1):
+        print(ans[y][x], end=" ")
+    print()
