e288c345a08d13d4b3906993c29210690605cf76
YongBum <dt10002@naver.com>
2023-08-18 18:00:35 +0900

    23.08.18

--- note.py
+++ note.py
@@ -1,44 +1,37 @@
 from sys import stdin, maxsize
-from heapq import heappush, heappop
 
 input = stdin.readline
 INF = maxsize
 
-n, m = map(int, input().split())  # n: 정점의 개수, m: 간선의 개수
-vertex = [[] for _ in range(n + 1)]
+n, m = map(int, input().split())  # n: 집하장의 개수, m: 경로의 개수
+mat = [[INF] * (n + 1) for _ in range(n + 1)]
+ans = [[INF] * (n + 1) for _ in range(n + 1)]
+for i in range(1, n + 1):
+    mat[i][i] = 0
+    ans[i][i] = "-"
 
 for _ in range(m):
-    a, b, c = map(int, input().split())  # a <-> b:가중치 c 양방향
-    vertex[a].append((b, c))
-    vertex[b].append((a, c))
+    a, b, t = map(int, input().split())  # a <-> b:시간 t 양방향
+    mat[a][b], mat[b][a] = t, t
+    ans[a][b], ans[b][a] = b, a
 
-p, q = map(int, input().split())  # p: 집 후보지의 개수, q: 편의점의 개수
-p_list = list(map(int, input().split()))  # p_list: 집 정점번호 리스트
-q_list = list(map(int, input().split()))  # q_list: 편의점 정점번호 리스트
+# 플로이드 워셜 알고리즘 구현
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if mat[i][j] > mat[i][k] + mat[k][j]:
+                mat[i][j] = mat[i][k] + mat[k][j]
+                # 초기화 값인 INF인 경우에만 첫 경로인 k로 갱신
+                ans[i][j] = ans[i][k]
 
-dist = [INF] * (n + 1)
-pq = []
-for store in q_list:
-    dist[store] = 0
-    heappush(pq, (0, store))
+# 정답 출력
+for y in range(1, n + 1):
+    for x in range(1, n + 1):
+        print(ans[y][x], end=" ")
+    print()
 
-ans = 0
-# 다익스트라 알고리즘 구현
-while pq:
-    min_dist, cur_v = heappop(pq)
-    if dist[cur_v] != min_dist:
-        continue
-
-    # 3개의 편의점 중에서 가장 먼저 집에 도착한다 -> 가장 짧은 거리 (우선 순위 큐 성질)
-    if cur_v in p_list:
-        ans = cur_v
-        break
-
-    for nxt_v, nxt_dist in vertex[cur_v]:
-        new_dist = dist[cur_v] + nxt_dist
-
-        if new_dist < dist[nxt_v]:
-            dist[nxt_v] = new_dist
-            heappush(pq, (new_dist, nxt_v))
-
-print(ans)
+print()
+for y in range(1, n + 1):
+    for x in range(1, n + 1):
+        print(mat[y][x], end=" ")
+    print()
