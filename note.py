146d1715001805f303f4d1f3ce54f03d123dc193
YongBum <dt10002@naver.com>
2023-08-31 10:20:27 +0900

    23.08.31

--- note.py
+++ note.py
@@ -2,10 +2,9 @@ from sys import stdin
 
 input = stdin.readline
 
-n = int(input().strip())  # n!
-
-ans = 1
+n = int(input().strip())
+ans_1 = 0
 for i in range(1, n + 1):
-    ans *= i
+    ans_1 += i
 
-print(ans)
+print(f"{ans_1}\n{ans_1 ** 2}\n{ans_1 ** 2}")
