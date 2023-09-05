74ed45d8fc6347bcc3d428ed5fd19b99bf8e86bb
YongBum <dt10002@naver.com>
2023-09-05 23:10:14 +0900

    23.09.05

--- note.py
+++ note.py
@@ -1,34 +1,36 @@
-from sys import stdin
+from sys import stdin, maxsize
+from heapq import heappush, heappop
 
 input = stdin.readline
+INF = maxsize
 
-t = int(input().strip())  # t: 테스크 케이스
 
-for _ in range(t):
-    # (x1, y1) -> 조규현의 좌표
-    # (x2, y2) -> 백승환의 좌표
-    # r1: 조규현이 계산한 류재명과의 거리
-    # r2: 백승환이 계산한 류재명과의 거리
-    x1, y1, r1, x2, y2, r2 = map(int, input().split())
-    p_p = (x2 - x1) ** 2 + (y2 - y1) ** 2
-    r1_p_r2 = (r1 + r2) ** 2
-    r1_m_r2 = abs(r1 - r2) ** 2
-    # 조규현과 백승환이 같은 좌표에 있는 경우
-    if x1 == x2 and y1 == y2:
-        # 있을 수 있는 위치가 0개인 경우
-        if r1 != r2:
-            print(0)
-        # 위치의 개수가 무한대인 경우
-        else:
-            print(-1)
-    # 조규현과 백승환이 다른 좌표에 있는 경우
-    else:
-        # 교점이 없는 경우
-        if p_p > r1_p_r2 or p_p < r1_m_r2:
-            print(0)
-        # 한 점에서 접하는 경우
-        elif p_p == r1_p_r2 or r1_m_r2 == p_p:
-            print(1)
-        # 두 점에서 만나는 경우
-        else:
-            print(2)
+# 다익스트라 알고리즘 구현
+def dijkstra(i, j):
+    dist = [INF] * (t + 1)
+    dist[i] = 0
+    pq = [(0, i)]
+
+    while pq:
+        min_dist, cur_v = heappop(pq)
+        if dist[cur_v] != min_dist:
+            continue
+
+        for nxt_v, nxt_dist in vertex[cur_v]:
+            new_dist = dist[cur_v] + nxt_dist
+            if new_dist < dist[nxt_v]:
+                dist[nxt_v] = new_dist
+                heappush(pq, (new_dist, nxt_v))
+
+    return dist[j]
+
+
+t, c, ts, te = map(int, input().split())  # t: 마을의 수, c: 연결 수 , ts: 시점, te: 종점
+vertex = [[] for _ in range(t + 1)]
+for _ in range(c):
+    r1, r2, c = map(int, input().split())
+    vertex[r1].append((r2, c))
+    vertex[r2].append((r1, c))
+
+# ts 마을에서 -> te 마을까지의 최단 거리를 출력
+print(dijkstra(ts, te))
