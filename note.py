29accc5b9c314600e55de3f6856e98f39c853871
YongBum <dt10002@naver.com>
2023-09-10 10:42:16 +0900

    23.09.10

--- note.py
+++ note.py
@@ -2,49 +2,33 @@ from sys import stdin
 
 input = stdin.readline
 
+n = int(input().strip())  # n: 집의 크기
+MAP = [list(map(int, input().split())) for _ in range(n)]
+dp = [[[0 for _ in range(n)] for _ in range(n)] for _ in range(3)]
 
-def dfs(cur_v):
-    global res
+# 0: 가로, 1: 대각선, 2: 세로
+dp[0][0][1] = 1  # 초기화
+for i in range(2, n):
+    # 벽이 아니라면 -> 가로로 파이프를 놓을 수 있다.
+    if MAP[0][i] == 0:
+        dp[0][0][i] = 1
+    # 벽을 만난다면 -> 더 이상, 가로로 파이프를 놓을 수 없다.
+    else:
+        break
 
-    for nxt_v in vertex[cur_v]:
-        # 방문이 가능하다면 -> dfs 탐색
-        if visited[nxt_v]:
-            visited[nxt_v] = False  # 방문 처리
-            res.append(nxt_v)
-            dfs(nxt_v)
+# dp 진행
+for i in range(1, n):
+    for j in range(1, n):
+        # 벽이 아니라면 -> dp 진행
+        if MAP[i][j] == 0:
+            # 가로로 놓을 수 있는 경우 (->->, \->)
+            dp[0][i][j] = dp[0][i][j - 1] + dp[1][i][j - 1]
 
+            # 세로로 놓을 수 있는 경우 ( | |, \ |)
+            dp[2][i][j] = dp[2][i - 1][j] + dp[1][i - 1][j]
 
-n = int(input().strip())  # n: 도시의 수
-m = int(input().strip())  # m: 계획에 속한 도시의 수
+            # 대각선으로 놓을 수 있는 경우 (공간이 필요) (-\, \ \, |\)
+            if MAP[i - 1][j] == 0 and MAP[i][j] == 0 and MAP[i][j - 1] == 0:
+                dp[1][i][j] = dp[0][i - 1][j - 1] + dp[1][i - 1][j - 1] + dp[2][i - 1][j - 1]
 
-vertex = [[] for _ in range(n + 1)]  # 친구 관계 -> 연결 리스트
-
-# 친구 관계 설정
-for i in range(1, n + 1):
-    rel = [0] + list(map(int, input().split()))
-    for j in range(1, n + 1):
-        if rel[j] == 1:
-            vertex[i].append(j)
-
-plan = list(map(int, input().split()))  # plan: 여행 계획한 도시 번호
-
-visited = [True] * (n + 1)
-flag = None
-for i in range(1, n + 1):
-    if visited[i]:
-        visited[i] = False  # 방문 처리
-        res = [i]
-        dfs(i)
-        flag = True
-        for elem in plan:
-            if elem not in res:
-                flag = False
-                break
-
-        if flag:
-            break
-
-if flag:
-    print("YES")
-else:
-    print("NO")
+print(dp[0][n - 1][n - 1] + dp[1][n - 1][n - 1] + dp[2][n - 1][n - 1])
