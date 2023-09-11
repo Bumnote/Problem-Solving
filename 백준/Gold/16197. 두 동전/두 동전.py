ca71814c3be0b627df16e7651c804a394fac93f0
DT <88239689+Bumnote@users.noreply.github.com>
2023-09-12 00:41:43 +0900

    [Gold IV] Title: 두 동전, Time: 244 ms, Memory: 164052 KB -BaekjoonHub

--- 백준/Gold/16197. 두 동전/두 동전.py
+++ 백준/Gold/16197. 두 동전/두 동전.py
@@ -23,20 +23,19 @@ def bfs(y0, x0, y1, x1):
     while dq:
         cy_0, cx_0, cy_1, cx_1, cur_cnt = dq.popleft()
 
-        if cur_cnt > 10:
+        # 문제 조건: 버튼을 10번보다 많이 누르게 되면 -1 출력
+        if cur_cnt > 9:
             return -1
 
-            # 순서: 왼쪽, 오른쪽, 위, 아래
+        # 순서: 왼쪽, 오른쪽, 위, 아래
         for dy, dx in ((0, -1), (0, 1), (-1, 0), (1, 0)):
             ny_0, nx_0, ny_1, nx_1 = cy_0 + dy, cx_0 + dx, cy_1 + dy, cx_1 + dx
             # 두 코인이 모두 나간 경우 -> 신경쓰지 않는다.
             if check_first_coin(ny_0, nx_0) and check_second_coin(ny_1, nx_1):
-                # print(F"모두 나간 경우 -> {ny_0} {nx_0} {ny_1} {nx_1}")
-                # print(f"{check_first_coin(ny_0, nx_0)} {check_second_coin(ny_1, nx_1)}")
                 continue
+
             # 두 코인 중 하나만 나간 경우 -> 움직인 횟수를 반환한다.
             elif check_first_coin(ny_0, nx_0) ^ check_second_coin(ny_1, nx_1):
-                # print(f"하나만 나간 경우 -> {ny_0} {nx_0} {ny_1} {nx_1}")
                 return cur_cnt + 1
 
             # 두 코인 모두 범위를 벗어나지 않는 경우 -> 계속 탐색
@@ -44,6 +43,7 @@ def bfs(y0, x0, y1, x1):
                 # 하나의 코인이라도 이미 방문했던 곳을 들린다면 -> 탐색하지 않는다.
                 if not visited[ny_0][nx_0][0] or not visited[ny_1][nx_1][1]:
                     continue
+
                 # 첫번째 코인의 경우 -> 벽이거나, 이미 방문한 곳이라면 멈춰있는다.
                 if MAP[ny_0][nx_0] == "#":
                     ny_0, nx_0 = cy_0, cx_0  # 원래 자리에 멈춰있는다.
@@ -52,7 +52,6 @@ def bfs(y0, x0, y1, x1):
                 if MAP[ny_1][nx_1] == "#":
                     ny_1, nx_1 = cy_1, cx_1
 
-                # print(f"추가되는 내용 -> {ny_0} {nx_0} {ny_1} {nx_1}")
                 # 좌표의 변화가 없는 경우 -> 추가하지 않는다.
                 if ny_0 == cy_0 and nx_0 == cx_0 and ny_1 == cy_1 and nx_1 == cx_1:
                     continue
@@ -74,6 +73,6 @@ for i in range(n):
 
 # 버튼 -> 왼쪽, 오른쪽, 위, 아래
 y0, x0, y1, x1 = *coins[0], *coins[1]
-# print(f"y0 = {y0} / x0 = {x0} / y1 = {y1} / x1 = {x1}")
 res = bfs(y0, x0, y1, x1)
+
 print(res if res <= 10 else -1)
