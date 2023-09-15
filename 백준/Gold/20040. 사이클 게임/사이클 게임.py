31243f9134ca7d1a0562be7fb457ddfedd0f37ff
DT <88239689+Bumnote@users.noreply.github.com>
2023-09-15 16:50:25 +0900

    [Gold IV] Title: 사이클 게임, Time: 804 ms, Memory: 125900 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.py
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.py
@@ -1,36 +1,44 @@
-from sys import stdin, setrecursionlimit
+import sys
 
-setrecursionlimit(10 ** 6)
-input = stdin.readline
+input = sys.stdin.readline
 
 
+# 1. 경로 압축
 def find(x):
-    if uf[x] == x:
-        return x
-
-    uf[x] = find(uf[x])
-    return uf[x]
+    if x != parent[x]:
+        parent[x] = find(parent[x])  # 재귀를 돌며 해당 부모를 최상위 노드로 설정
+    return parent[x]
 
 
-def union(a, b):
-    a, b = find(a), find(b)
-    uf[a] = b
+# 2. union by rank 처리
+def union_(a, b):
+    a = find(a)
+    b = find(b)
+
+    if a == b:
+        return
+    # 만약 랭크 a가 b보다 더 깊다면
+    if rank[a] > rank[b]:
+        parent[b] = a  # b에 a를 붙인다.
+    elif rank[a] < rank[b]:  # 만약 랭크 b가 랭크 a보다 더 깊다면
+        parent[a] = b  # a에 b를 붙인다.
+    else:  # 둘의 랭크가 같다면
+        rank[b] += 1  # b의 랭크깊이를 1개 추가한 뒤
+        parent[a] = b  # b에 a를 붙인다.
 
 
-n, m = map(int, input().split())  # n: 점의 개수, m: 차례의 수
-uf = [i for i in range(n)]  # union-find list
+N, M = map(int, input().split())
 
+parent = [i for i in range(N)]
+rank = [0] * N  # union by rank 처리를 위한 랭크 기록함수
 ans = 0
-for j in range(1, m + 1):
-    u, v = map(int, input().split())  # u <-> v: 연결
-    # 이미 같은 집합인데 다시 연결한다면 -> 사이클 발생
-    if find(u) == find(v) and ans == 0:
-        ans = j
-        continue
+for i in range(M):
+    a, b = map(int, input().split())
 
-    # 이미 갱신이 되었다면 -> union 작업을 하지 않아도 된다.
-    if ans != 0:
-        continue
-    union(u, v)
+    if not ans:  # 답이 아직 나오지 않았다면
+        if find(a) == find(b):  # 만약 싸이클이 형성되어 있다면
+            ans = i + 1  # 해당 순번을 입력
+        else:  # 싸이클이 형성되어있지 않다면
+            union_(a, b)  # 두 노드를 유니온
 
 print(ans)
