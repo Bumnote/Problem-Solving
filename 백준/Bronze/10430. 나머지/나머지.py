e81c13ae08b4a30e719ce4f6f2a287ab6696a449
DT <88239689+Bumnote@users.noreply.github.com>
2024-08-27 14:11:35 +0900

    [Bronze V] Title: 나머지, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.py
@@ -0,0 +1,10 @@
+from sys import stdin
+
+input = stdin.readline
+
+a, b, c = map(int, input().split())
+
+print((a + b) % c)
+print(((a % c) + (b % c)) % c)
+print((a * b) % c)
+print(((a % c) * (b % c)) % c)
