295dcc73aa5c4eab199e2824cf801816f7f3f25e
Yongbum Kim <88239689+Bumnote@users.noreply.github.com>
2024-09-17 14:46:42 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.py
@@ -0,0 +1,12 @@
+from sys import stdin
+
+input = stdin.readline
+
+n = int(input().rstrip())
+
+dp = [0] * 16
+dp[0] = 2
+for i in range(1, 16):
+    dp[i] = dp[i - 1] + dp[i - 1] - 1
+
+print(dp[n] ** 2)
