80c294b9131bcbfdeec349013d894b6bea7126dc
Yongbum Kim <88239689+Bumnote@users.noreply.github.com>
2024-11-19 18:50:32 +0900

    [Silver IV] Title: 스택 2, Time: 1156 ms, Memory: 281152 KB -BaekjoonHub

--- Java/백준/Silver/28278. 스택 2/스택 2.java
+++ Java/백준/Silver/28278. 스택 2/스택 2.java
@@ -9,7 +9,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        Stack<Integer> stk = new Stack<>();
+        Deque<Integer> stk = new ArrayDeque<>();
 
         int n, command, value, flag;
         st = new StringTokenizer(br.readLine());
@@ -17,36 +17,35 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            if (st.countTokens() == 2) {
-                command = Integer.parseInt(st.nextToken());
-                value = Integer.parseInt(st.nextToken());
-                stk.push(value);
-            } else {
-                command = Integer.parseInt(st.nextToken());
-                switch (command) {
+            command = Integer.parseInt(st.nextToken());
+            switch (command) {
+                case 1:
+                    value = Integer.parseInt(st.nextToken());
+                    stk.push(value);
+                    break;
+                    
+                case 2:
+                    if (stk.isEmpty())
+                        bw.write(-1 + "\n");
+                    else
+                        bw.write(stk.pop() + "\n");
+                    break;
 
-                    case 2:
-                        if (stk.isEmpty())
-                            bw.write(-1 + "\n");
-                        else
-                            bw.write(stk.pop() + "\n");
-                        break;
+                case 3:
+                    bw.write(stk.size() + "\n");
+                    break;
 
-                    case 3:
-                        bw.write(stk.size() + "\n");
-                        break;
+                case 4:
+                    flag = stk.isEmpty() ? 1 : 0;
+                    bw.write(flag + "\n");
+                    break;
 
-                    case 4:
-                        flag = stk.isEmpty() ? 1 : 0;
-                        bw.write(flag + "\n");
-                        break;
-                    case 5:
-                        if (stk.isEmpty())
-                            bw.write(-1 + "\n");
-                        else
-                            bw.write(stk.peek() + "\n");
-                        break;
-                }
+                case 5:
+                    if (stk.isEmpty())
+                        bw.write(-1 + "\n");
+                    else
+                        bw.write(stk.peek() + "\n");
+                    break;
             }
         }
 
