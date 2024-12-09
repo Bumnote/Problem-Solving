0917e56a25fc313f2c430e66913b9ecb854ad25f
Yongbum Kim <88239689+Bumnote@users.noreply.github.com>
2024-12-09 20:47:21 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 100 ms, Memory: 109544 KB -BaekjoonHub

---
+++ PyPy3/백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.py
@@ -0,0 +1,20 @@
+from sys import stdin
+
+input = stdin.readline
+
+n = int(input().rstrip())
+lst = list(map(int, input().split()))
+dp = [1 for _ in range(n)]
+
+LEN = 1
+for i in range(1, n):
+    j = i - 1
+    # 현재 숫자보다 작은 인덱스에 대한 dp값을 찾는다.
+    while j >= 0:
+        if lst[j] > lst[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+        j -= 1
+
+    LEN = max(LEN, dp[i])
+
+print(LEN)
