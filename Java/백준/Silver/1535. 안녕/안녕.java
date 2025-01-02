a0cf06ff434c1fcccde2e3708675aef3f11fd030
Yongbum Kim <88239689+Bumnote@users.noreply.github.com>
2025-01-02 19:54:43 +0900

    [Silver II] Title: 안녕, Time: 64 ms, Memory: 11588 KB -BaekjoonHub

--- Java/백준/Silver/1535. 안녕/안녕.java
+++ Java/백준/Silver/1535. 안녕/안녕.java
@@ -6,50 +6,38 @@ class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
 
-    static int N, power = 100, happiness = 0, ans = 0;
-    static boolean[] visited;
-    static ArrayList<Integer> lst = new ArrayList<>();
+    static int N;
     static int[] L;
     static int[] J;
+    static int[][] dp;
 
     public static void main(String[] args) throws IOException {
 
-        N = Integer.parseInt(br.readLine()); // N: 사람 수
+        N = Integer.parseInt(br.readLine());
 
-        L = new int[N];
-        J = new int[N];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++)
-            L[i] = Integer.parseInt(st.nextToken()); // 잃는 체력
+        L = new int[N + 1];
+        J = new int[N + 1];
 
         st = new StringTokenizer(br.readLine());
-        for (int j = 0; j < N; j++)
-            J[j] = Integer.parseInt(st.nextToken()); // 얻는 기쁨
+        for (int i = 1; i <= N; i++)
+            L[i] = Integer.parseInt(st.nextToken());
 
-        visited = new boolean[N];
-        bt(lst, 0);
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++)
+            J[i] = Integer.parseInt(st.nextToken());
 
-        System.out.println(ans);
-
-    }
-
-    private static void bt(ArrayList<Integer> lst, int total) {
-
-        ans = Math.max(ans, total);
-
-        for (int i = 0; i < N; i++) {
-            if (!lst.isEmpty() && lst.get(lst.size() - 1) >= i)
-                continue;
-
-            if (!visited[i] && power - L[i] > 0) {
-                visited[i] = true; // 방문 처리
-                lst.add(i);
-                power -= L[i];
-                bt(lst, total + J[i]);
-                power += L[i];
-                lst.remove(lst.size() - 1);
-                visited[i] = false; // 복원 처리
+        dp = new int[N + 1][101];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j < 100; j++) {
+                int weight = L[i];
+                int value = J[i];
+                if (j < weight)
+                    dp[i][j] = dp[i - 1][j];
+                else
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weight] + value);
             }
         }
+
+        System.out.println(dp[N][99]);
     }
 }
