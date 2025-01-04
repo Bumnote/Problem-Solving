9726955bfdda8d1716933668cd04b7bb8bc6e26e
Yongbum Kim <88239689+Bumnote@users.noreply.github.com>
2025-01-04 16:45:34 +0900

    [Gold IV] Title: 달팽이팽이, Time: 92 ms, Memory: 12908 KB, Score: 5 point -BaekjoonHub

---
+++ Java/백준/Gold/23890. 달팽이팽이/달팽이팽이.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+    static long R;
+    static long y, x;
+
+    public static void main(String[] args) {
+
+        R = sc.nextInt(); // R: 반지름
+
+        y = R - 1;
+        x = (long) Math.sqrt(2 * R - 1);
+        x = (x * x + y * y) == R * R ? x - 1 : x;
+
+        System.out.printf("%d %d", y, x);
+    }
+}
