b4d5abd06d622d935d81520a426bef28b51598ae
Yongbum Kim <88239689+Bumnote@users.noreply.github.com>
2025-02-11 14:33:39 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 212 ms, Memory: 18076 KB -BaekjoonHub

---
+++ Java/백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static String line;
+    static int N, size;
+    static int[] tree = new int[10_001];
+    static ArrayList<Integer> ans;
+
+
+    public static void main(String[] args) throws IOException {
+
+        init();
+
+        solve();
+
+    }
+
+    private static void solve() {
+
+        postOrder(0, size);
+
+        System.out.println(sb.toString());
+    }
+
+
+    private static void postOrder(int s, int e) {
+        // 종료 조건
+        if (s >= e)
+            return;
+
+        int mid = s + 1;
+
+        while (mid < e) {
+            if (tree[s] < tree[mid])
+                break;
+            mid++;
+        }
+
+        postOrder(s + 1, mid);
+        postOrder(mid, e);
+        sb.append(tree[s]).append("\n");
+    }
+
+    private static void init() throws IOException {
+
+        // 입력 부분
+        line = null;
+
+        size = 0;
+        ans = new ArrayList<>();
+        while ((line = br.readLine()) != null) {
+            N = Integer.parseInt(line);
+            tree[size++] = N;
+        }
+
+        br.close();
+    }
+}
