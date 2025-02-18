d0ab02cb7e0cac22487ea0e0118b822069f42115
Yongbum Kim <88239689+Bumnote@users.noreply.github.com>
2025-02-19 01:13:46 +0900

    [Gold V] Title: 평범한 배낭, Time: 132 ms, Memory: 51240 KB -BaekjoonHub

--- Java/백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ Java/백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,44 +1,50 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Main {
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static int N, K, ans = 0;
-
-    static int[] W;
-    static int[] V;
+    static int N, K;
+    static int[] W, V;
     static int[][] dp;
 
     public static void main(String[] args) throws IOException {
 
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+        init();
 
-        W = new int[N + 1];
-        V = new int[N + 1];
-        dp = new int[N + 1][K + 1];
-        for (int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine());
-            W[i] = Integer.parseInt(st.nextToken());
-            V[i] = Integer.parseInt(st.nextToken());
-        }
+        solve();
+
+    }
+
+    private static void solve() {
 
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= K; j++) {
-                int weight = W[i];
-                int value = V[i];
-
-                if (j < weight)
-                    dp[i][j] = dp[i - 1][j];
-                else
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weight] + value);
+                if (j < W[i]) dp[i][j] = dp[i - 1][j];
+                else dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - W[i]] + V[i]);
             }
         }
 
         System.out.println(dp[N][K]);
     }
 
+    private static void init() throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken()); // N: 물건의 개수
+        K = Integer.parseInt(st.nextToken()); // K: 최대 담을 있는 무게
+
+        W = new int[N + 1];
+        V = new int[N + 1];
+        dp = new int[N + 1][K + 1];
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            W[i] = Integer.parseInt(st.nextToken()); // W: 무게
+            V[i] = Integer.parseInt(st.nextToken()); // V: 가치
+        }
+
+        br.close();
+    }
 }
