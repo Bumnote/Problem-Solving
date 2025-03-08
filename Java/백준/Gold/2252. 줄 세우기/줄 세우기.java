0dd532a3327fee4ac5fbf93403433a1db940ef00
Yongbum Kim <88239689+Bumnote@users.noreply.github.com>
2025-03-09 01:06:38 +0900

    [Gold III] Title: 줄 세우기, Time: 360 ms, Memory: 45836 KB -BaekjoonHub

---
+++ Java/백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  static int N, M, A, B;
+  static int[] edgeCnt;
+  static ArrayList<ArrayList<Integer>> vertex;
+
+  public static void main(String[] args) throws IOException {
+    init();
+    solve();
+  }
+
+  private static void init() throws IOException {
+
+    // 입력 부분
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+
+    st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+
+    vertex = new ArrayList<>();
+    edgeCnt = new int[N + 1]; // 진입차수 배열
+    for (int i = 0; i <= N; i++) {
+      vertex.add(new ArrayList<>());
+    }
+
+    for (int i = 0; i < M; i++) {
+      st = new StringTokenizer(br.readLine());
+      A = Integer.parseInt(st.nextToken());
+      B = Integer.parseInt(st.nextToken());
+      // A -> B: 단방향 그래프
+      vertex.get(A).add(B);
+      edgeCnt[B]++;
+    }
+
+    br.close();
+  }
+
+  private static void solve() {
+
+    StringBuilder sb = new StringBuilder();
+
+    Queue<Integer> dq = new ArrayDeque<>();
+
+    for (int i = 1; i <= N; i++) {
+      // 진입 차수가 0인 요소 추가
+      if (edgeCnt[i] == 0) {
+        dq.offer(i);
+      }
+    }
+
+    while (!dq.isEmpty()) {
+
+      int curr = dq.poll();
+
+      // 정답 양식 저장
+      sb.append(curr).append(" ");
+
+      for (Integer nxt : vertex.get(curr)) {
+        edgeCnt[nxt]--;
+        // 진입차수가 0이 된 경우 -> dq 추가
+        if (edgeCnt[nxt] == 0) {
+          dq.offer(nxt);
+        }
+      }
+    }
+
+    // 정답 출력
+    System.out.println(sb);
+  }
+
+}
