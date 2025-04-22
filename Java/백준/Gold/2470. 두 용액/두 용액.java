00f4704739f412de14a1e01b885ff2c18cbed92f
Yongbum Kim <88239689+Bumnote@users.noreply.github.com>
2025-04-22 17:09:03 +0900

    [Gold V] Title: 두 용액, Time: 252 ms, Memory: 31484 KB -BaekjoonHub

--- Java/백준/Gold/2470. 두 용액/두 용액.java
+++ Java/백준/Gold/2470. 두 용액/두 용액.java
@@ -1,63 +1,77 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static int N;
-    static int[] arr;
+  static int N;
+  static int[] arr;
 
-    public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException {
+    init();
+    solve();
+  }
 
-        N = Integer.parseInt(br.readLine());
+  private static void init() throws IOException {
 
-        st = new StringTokenizer(br.readLine());
-        arr = new int[N];
-        for (int i = 0; i < N; i++)
-            arr[i] = Integer.parseInt(st.nextToken());
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
 
-        Arrays.sort(arr);
-
-        int[] res = bs();
-        System.out.printf("%d %d\n", res[0], res[1]);
-
+    // 입력 부분
+    N = Integer.parseInt(br.readLine()); // N: 용액의 수
+    arr = new int[N];
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < N; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
     }
 
-    private static int[] bs() {
+    br.close();
+  }
 
-        int left = 0;
-        int right = N - 1;
+  private static void solve() {
 
-        int MIN = Integer.MAX_VALUE;
-        int[] res = null;
+    Arrays.sort(arr); // 정렬
 
-        // 모든 숫자가 양수인 경우
-        if (arr[left] > 0)
-            return new int[]{arr[left], arr[left + 1]};
+    // 모두 양수인 경우 -> 처음 두 숫자가 정답
+    if (arr[0] > 0) {
+      System.out.printf("%d %d", arr[0], arr[1]);
+    }
+    // 모두 음수인 경우 -> 마지막 두 숫자가 정답
+    else if (arr[arr.length - 1] < 0) {
+      System.out.printf("%d %d", arr[arr.length - 2], arr[arr.length - 1]);
+    }
+    // 양수와 음수가 섞여있는 경우 -> 투포인터 적용
+    else {
+      int[] ans = bs();
+      System.out.printf("%d %d", ans[0], ans[1]);
+    }
+  }
 
-        // 모든 숫자가 음수인 경우
-        if (arr[right] < 0)
-            return new int[]{arr[right - 1], arr[right]};
+  private static int[] bs() {
 
-        // 양수와 음수 모두 존재하는 경우
-        while (left < right) {
+    int left = 0;
+    int right = arr.length - 1;
 
-            int sum = arr[left] + arr[right];
-            if (Math.abs(sum) < MIN) {
-                MIN = Math.abs(sum);
-                res = new int[]{arr[left], arr[right]};
-            }
+    int MIN = Integer.MAX_VALUE;
+    int[] res = new int[]{arr[left], arr[right]};
+    while (left < right) {
+      int diff = (arr[left] + arr[right]);
+      // 0과의 거리가 갱신된다면 -> 갱신한다.
+      if (Math.abs(diff) < MIN) {
+        MIN = Math.abs(diff);
+        res[0] = arr[left];
+        res[1] = arr[right];
+      }
 
-            if (sum == 0)
-                return new int[]{arr[left], arr[right]};
-            else if (sum < 0) {
-                left++;
-            } else
-                right--;
-        }
-
-        return res;
+      // 차이가 음수라면 -> 음수를 줄인다.
+      if (diff < 0) {
+        left++;
+      }
+      // 차이가 음이 아닌 수라면 -> 양수를 줄인다.
+      else {
+        right--;
+      }
     }
 
+    return res;
+  }
 }
