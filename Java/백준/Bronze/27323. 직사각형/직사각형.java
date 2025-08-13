5e07e93ceb966a330b759c325013f22687642798
Yongbum Kim <88239689+Bumnote@users.noreply.github.com>
2025-08-13 19:30:39 +0900

    [Bronze V] Title: 직사각형, Time: 88 ms, Memory: 12824 KB -BaekjoonHub

---
+++ Java/백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    private static Scanner sc = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+     
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        System.out.println(a * b);
+    } 
+}
