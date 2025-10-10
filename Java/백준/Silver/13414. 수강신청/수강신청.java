8420c384a37af4304919f9c9156645cd40671bd4
Yongbum Kim <88239689+Bumnote@users.noreply.github.com>
2025-10-11 00:49:44 +0900

    [Silver III] Title: 수강신청, Time: 532 ms, Memory: 68872 KB -BaekjoonHub

--- Java/백준/Silver/13414. 수강신청/수강신청.java
+++ Java/백준/Silver/13414. 수강신청/수강신청.java
@@ -1,44 +1,63 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        int K = Integer.parseInt(input[0]), L = Integer.parseInt(input[1]);
-        HashMap<String, Integer> map = new HashMap<>();
+class Main {
 
-        for (int i = 0; i < L; i++) {
-            String id = br.readLine();
-            if (map.containsKey(id)) {
-                map.remove(id);
-                map.put(id, i);
-            } else {
-                map.put(id, i);
-            }
-        }
+  static class Student implements Comparable<Student> {
+    String studentNumber;
+    int index;
 
-        PriorityQueue<Student> pq = new PriorityQueue<>((a, b) -> a.idx - b.idx);
-        for (String s : map.keySet()) {
-            pq.offer(new Student(s, map.get(s)));
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < K; i++) {
-            if (!pq.isEmpty()) {
-                sb.append(pq.poll().id + "\n");
-            }
-        }
-        System.out.println(sb);
+    Student(String studentNumber, int index) {
+      this.studentNumber = studentNumber;
+      this.index = index;
     }
 
-    static class Student {
-        String id;
-        int idx;
-
-        public Student(String id, int idx) {
-            this.id = id;
-            this.idx = idx;
-        }
+    @Override
+    public int compareTo(Student o) {
+      return Integer.compare(this.index, o.index);
     }
+  }
+
+  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static final StringBuilder sb = new StringBuilder();
+  private static StringTokenizer st;
+  private static int k, l;
+  private static final Map<String, Integer> map = new HashMap<>();
+  private static final PriorityQueue<Student> pq = new PriorityQueue<>();
+
+  public static void main(String[] args) throws IOException {
+    init();
+    solve();
+  }
+
+  private static void init() throws IOException {
+
+    st = new StringTokenizer(br.readLine());
+    k = Integer.parseInt(st.nextToken());
+    l = Integer.parseInt(st.nextToken());
+
+    for (int i = 0; i < l; i++) {
+      String studentNumber = br.readLine();
+      // 이미 한번 수강 신청을 한 경우 -> 제거 인덱스 정보 저장
+      map.put(studentNumber, i);
+    }
+    br.close();
+  }
+
+  private static void solve() {
+
+    for (Map.Entry<String, Integer> entry : map.entrySet()) {
+      pq.offer(new Student(entry.getKey(), entry.getValue()));
+    }
+
+    int count = 0;
+    while (!pq.isEmpty() && count < k) {
+      Student student = pq.poll();
+      sb.append(student.studentNumber).append("\n");
+      count++;
+    }
+
+    System.out.print(sb);
+    sb.setLength(0);
+  }
 }
