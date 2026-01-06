2be000e89aaa8cbaea987ab65912fda6660529fe
YongBum Kim <dt10002@naver.com>
2026-01-06 15:27:04 +0900

    [Gold IV] Title: 사촌, Time: 604 ms, Memory: 156644 KB -BaekjoonHub

--- Java/백준/Gold/9489. 사촌/사촌.java
+++ Java/백준/Gold/9489. 사촌/사촌.java
@@ -1,9 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 class Main {
@@ -12,8 +8,8 @@ class Main {
   private static final StringBuilder sb = new StringBuilder();
   private static StringTokenizer st;
 
-  public static Map<Integer, List<Integer>> tree = new HashMap<>();
-  public static int n, k;
+  public static int[] arr, parent;
+  public static int n, k, targetParent, targetIdx;
 
   public static void main(String[] args) throws Exception {
     while (true) {
@@ -31,80 +27,48 @@ class Main {
   }
 
   private static void init() throws Exception {
-
     st = new StringTokenizer(br.readLine());
-    int[] arr = new int[st.countTokens()];
-    for (int i = 0; i < arr.length; i++) {
+    arr = new int[n + 1];
+    parent = new int[n + 1];
+    for (int i = 1; i <= n; i++) {
       arr[i] = Integer.parseInt(st.nextToken());
+      if (arr[i] == k) {
+        targetIdx = i;
+      }
     }
 
-    initTree(arr);
+    initParentInfo(arr);
   }
 
   private static void solve() {
 
-    // k 부모 찾기
-    int parentNode = -1;
-    for (Map.Entry<Integer, List<Integer>> entry : tree.entrySet()) {
-      if (entry.getValue().contains(k)) {
-        parentNode = entry.getKey();
-        break;
+    int uncleCount = 0;
+    for (int i = 1; i <= n; i++) {
+      // 부모가 같지 않으면서, 조상이 같은 경우(즉, 형제인 경우) -> 사촌이다.
+      if (parent[i] != parent[targetIdx] && parent[parent[i]] == parent[targetParent]) {
+        uncleCount++;
       }
     }
 
-    // 부모가 없으면 -> 사촌 없다
-    if (parentNode == -1) {
-      sb.append(0).append("\n");
-      return;
-    }
-
-    // 조부모 찾기
-    int grandParent = -1;
-    for (Map.Entry<Integer, List<Integer>> entry : tree.entrySet()) {
-      if (entry.getValue().contains(parentNode)) {
-        grandParent = entry.getKey();
-        break;
-      }
-    }
-
-    // 조부모가 없으면 -> 사촌이 없다.
-    if (grandParent == -1) {
-      sb.append(0).append("\n");
-      return;
-    }
-
-    // 3. 사촌 계산
-    int cousinCount = 0;
-    for (int uncle : tree.get(grandParent)) {
-      if (uncle == parentNode) {
-        continue;
-      }
-      cousinCount += tree.getOrDefault(uncle, new ArrayList<>()).size();
-    }
-
-    sb.append(cousinCount).append("\n");
+    sb.append(uncleCount).append('\n');
   }
 
-  private static void initTree(int[] arr) {
-    tree.clear();
+  private static void initParentInfo(int[] arr) {
 
-    int ROOT = arr[0];
-    tree.put(ROOT, new ArrayList<>());
+    arr[0] = -1;
+    parent[0] = -1;
 
     int idx = 0;
-    List<Integer> lst = new ArrayList<>();
-    for (int i = 1; i < arr.length; i++) {
-      lst.add(arr[i]);
-      // 마지막 원소인 경우
-      if (i == arr.length - 1) {
-        tree.put(arr[idx], new ArrayList<>(lst));
-        lst.clear();
-        break;
+    targetParent = -1;
+    for (int i = 1; i <= n; i++) {
+      parent[i] = idx;
+      if (arr[i] == k) {
+        targetParent = parent[i];
       }
-      // 마지막 원소가 아니면서 같은 묶음이 깨지는 경우
-      if (Math.abs(arr[i] - arr[i + 1]) > 1) {
-        tree.put(arr[idx++], new ArrayList<>(lst));
-        lst.clear();
+
+      // 마지막 원소가 아니면서, 연속된 숫자가 아니라면 -> 새로운 부모 설정
+      if (i != n && arr[i + 1] - arr[i] > 1) {
+        idx++;
       }
     }
   }
