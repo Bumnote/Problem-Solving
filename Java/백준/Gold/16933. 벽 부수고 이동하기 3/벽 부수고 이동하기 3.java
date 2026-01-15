021d6941ae7fc73f2b932104247974d17f7e40db
YongBum Kim <dt10002@naver.com>
2026-01-16 02:47:07 +0900

    [Gold I] Title: 벽 부수고 이동하기 3, Time: 3932 ms, Memory: 700368 KB -BaekjoonHub

--- Java/백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.java
+++ Java/백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.java
@@ -1,41 +1,35 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
 
 class Main {
 
   static class Node {
 
-    int y;
-    int x;
-    int dist;
-    int destroyedWallCount;
-    int flag;
+    int y, x, dist, crush, day;
 
-    public Node(int y, int x, int dist, int destroyedWallCount, int flag) {
+    Node(int y, int x, int dist, int crush, int day) {
       this.y = y;
       this.x = x;
       this.dist = dist;
-      this.destroyedWallCount = destroyedWallCount;
-      this.flag = flag;
+      this.crush = crush;
+      this.day = day;
     }
   }
 
-  private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-  private static final StringBuilder sb = new StringBuilder();
-  private static StringTokenizer st;
-
-  private static int n, m, k;
-  private static int[][] map;
-  private static final int[] dys = {-1, 1, 0, 0}, dxs = {0, 0, -1, 1};
+  static int n, m, k;
+  static int[][] map;
+  static boolean[][][][] visited;
+  static final int[] dys = {-1, 1, 0, 0};
+  static final int[] dxs = {0, 0, -1, 1};
 
   public static void main(String[] args) throws Exception {
-    init();
-    solve();
-  }
 
-  private static void init() throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
 
-    st = new StringTokenizer(br.readLine());
     n = Integer.parseInt(st.nextToken());
     m = Integer.parseInt(st.nextToken());
     k = Integer.parseInt(st.nextToken());
@@ -47,57 +41,53 @@ class Main {
         map[i][j] = line.charAt(j) - '0';
       }
     }
-    br.close();
+
+    System.out.print(bfs());
   }
 
-  private static void solve() {
-    // (1, 1) -> (n, m)
-    boolean[][][][] visited = new boolean[n][m][k + 1][2];
-    visited[0][0][0][0] = true; // 처음 시작점 방문 처리
+  static int bfs() {
+    visited = new boolean[n][m][k + 1][2];
+    Deque<Node> q = new ArrayDeque<>();
 
-    Deque<Node> dq = new ArrayDeque<>();
-    dq.offer(new Node(0, 0, 1, 0, 0));
+    q.offer(new Node(0, 0, 1, 0, 0));
+    visited[0][0][0][0] = true;
 
-    while (!dq.isEmpty()) {
-      Node curr = dq.poll();
+    while (!q.isEmpty()) {
+      Node cur = q.poll();
 
-      if (curr.y == n - 1 && curr.x == m - 1) {
-        System.out.print(curr.dist);
-        return;
+      if (cur.y == n - 1 && cur.x == m - 1) {
+        return cur.dist;
       }
 
+      int nextDay = 1 - cur.day;
+
       for (int d = 0; d < 4; d++) {
-        int ny = curr.y + dys[d];
-        int nx = curr.x + dxs[d];
+        int ny = cur.y + dys[d];
+        int nx = cur.x + dxs[d];
 
         if (ny < 0 || ny >= n || nx < 0 || nx >= m) {
           continue;
         }
 
-        // 범위를 넘지 않고, 다음 위치, 벽을 부순 개수, 낮/밤 상태가 방문하지 않은 상태라면 -> 탐색
-        if (!visited[ny][nx][curr.destroyedWallCount][curr.flag]) {
-          // 다음 위치가 빈 칸이라면 -> 이동
-          if (map[ny][nx] == 0) {
-            visited[ny][nx][curr.destroyedWallCount][curr.flag] = true;
-            dq.offer(new Node(ny, nx, curr.dist + 1, curr.destroyedWallCount, 1 - curr.flag));
+        if (map[ny][nx] == 0) {
+          if (!visited[ny][nx][cur.crush][nextDay]) {
+            visited[ny][nx][cur.crush][nextDay] = true;
+            q.offer(new Node(ny, nx, cur.dist + 1, cur.crush, nextDay));
           }
-          // 다음 위치가 벽이라면 -> 벽을 부순 개수 판단
-          else if (curr.destroyedWallCount < k && !visited[ny][nx][curr.destroyedWallCount + 1][1 - curr.flag]) {
-            // 낮인 경우 -> 벽을 부수고 이동
-            if (curr.flag == 0) {
-              visited[ny][nx][curr.destroyedWallCount + 1][1 - curr.flag] = true;
-              dq.offer(new Node(ny, nx, curr.dist + 1, curr.destroyedWallCount + 1, 1 - curr.flag));
-            }
-            // 밤인 경우 -> 벽을 부술 수 없으므로, 원래 자리 유지
-            else {
-              visited[curr.y][curr.x][curr.destroyedWallCount][1 - curr.flag] = true;
-              dq.offer(new Node(curr.y, curr.x, curr.dist + 1, curr.destroyedWallCount, 1 - curr.flag));
-            }
+        } else if (map[ny][nx] == 1 && cur.day == 0 && cur.crush < k) {
+          if (!visited[ny][nx][cur.crush + 1][nextDay]) {
+            visited[ny][nx][cur.crush + 1][nextDay] = true;
+            q.offer(new Node(ny, nx, cur.dist + 1, cur.crush + 1, nextDay));
           }
         }
       }
+
+      if (!visited[cur.y][cur.x][cur.crush][nextDay]) {
+        visited[cur.y][cur.x][cur.crush][nextDay] = true;
+        q.offer(new Node(cur.y, cur.x, cur.dist + 1, cur.crush, nextDay));
+      }
     }
 
-    System.out.print(-1);
+    return -1;
   }
 }
