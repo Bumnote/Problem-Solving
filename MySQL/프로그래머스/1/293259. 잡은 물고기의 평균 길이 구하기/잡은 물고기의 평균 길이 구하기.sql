1251d15faac392af9a5cadbbd3a8217af4cabb7c
YongBum Kim <dt10002@naver.com>
2026-02-20 20:28:57 +0900

    [level 1] Title: 잡은 물고기의 평균 길이 구하기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/1/293259. 잡은 물고기의 평균 길이 구하기/잡은 물고기의 평균 길이 구하기.sql
+++ MySQL/프로그래머스/1/293259. 잡은 물고기의 평균 길이 구하기/잡은 물고기의 평균 길이 구하기.sql
@@ -1,10 +1,3 @@
-with fish_length as (
-    select case 
-            when length is not null then length
-            when length <= 10 or length is null then 10
-           end as 'length'
-    from fish_info 
-)
-
-select round(avg(l.length), 2) as 'AVERAGE_LENGTH'
-from fish_length l  
+select round(avg(length), 2) as "AVERAGE_LENGTH"
+from (select ifnull(length, 10) as "length"
+      from fish_info) as fish_length_info
